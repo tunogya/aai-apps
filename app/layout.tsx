426e873df45c52b2d53dcfcd7b94382136cacf8b
tunogya <tunogya@qq.com>
2023-09-01 00:50:09 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,18 +2,50 @@ import './globals.css'
 import type {Metadata} from 'next'
 import "cal-sans";
 import { Inter } from 'next/font/google'
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Abandon AI',
   description: 'Powered by OpenAI',
+  viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no',
+  applicationName: 'Abandon AI',
+  themeColor: '#fff',
+  openGraph: {
+    images: '/apple-touch-icon.png',
+    title: 'Abandon AI',
+  },
+  manifest: '/manifest.json',
 }
 
 export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
     <body className={`${inter.className} h-screen w-screen`}>
+    <Script src={'https://www.googletagmanager.com/gtag/js?id=G-EDPQ3K7EN8'}/>
+    <Script id='google-tag-manager' strategy='afterInteractive'>
+      {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                
+                gtag('config', 'G-EDPQ3K7EN8');
+              `}
+    </Script>
+    <Script id={'sw'}>
+      {`
+                if ('serviceWorker' in navigator) {
+                  window.addEventListener('load', function() {
+                    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+                      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                    }, function(err) {
+                      console.log('ServiceWorker registration failed: ', err);
+                    });
+                  });
+                }
+        `}
+    </Script>
     {children}
     </body>
     </html>
---
+++ public/sw.js
@@ -0,0 +1,51 @@
+'use strict'
+const cacheStorageKey = 'abandon.ai';
+
+const cacheList = [
+  '/',
+  "index.html"
+];
+
+self.addEventListener('install', function(e) {
+  console.log('Cache event!')
+  e.waitUntil(
+      caches.open(cacheStorageKey).then(function(cache) {
+        console.log('Adding to Cache:', cacheList)
+        return cache.addAll(cacheList)
+      }).then(function() {
+        console.log('Skip waiting!')
+        return self.skipWaiting()
+      })
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  console.log('Activate event')
+  e.waitUntil(
+      Promise.all(
+          caches.keys().then(cacheNames => {
+            return cacheNames.map(name => {
+              if (name !== cacheStorageKey) {
+                return caches.delete(name)
+              }
+            })
+          })
+      ).then(() => {
+        console.log('Clients claims.')
+        return self.clients.claim()
+      })
+  )
+})
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+      caches.match(e.request).then(function(response) {
+        if (response != null) {
+          console.log('Using cache for:', e.request.url)
+          return response
+        }
+        console.log('Fallback to fetch:', e.request.url)
+        return fetch(e.request.url)
+      })
+  )
+})
