d15de81e171995046abdf4bd5e5819d007890dc7
tunogya <tunogya@qq.com>
2023-09-01 08:06:23 +0800

    update

---
+++ app/components/Menu/MenuList.tsx
@@ -0,0 +1,53 @@
+'use client';
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+
+const menu = [
+  {
+    name: "Persona",
+    path: "/persona",
+    children: [],
+  },
+  {
+    name: "Discuss",
+    path: "/discuss",
+    children: [],
+  },
+  {
+    name: "Whisper",
+    path: "/whisper",
+    children: [],
+  },
+  {
+    name: "Activity",
+    path: "/activity",
+    children: [],
+  },
+  {
+    name: "Settings",
+    path: "/settings",
+    children: [],
+  },
+  {
+    name: "Help & Support",
+    path: "/help",
+    children: [],
+  },
+]
+
+export default function Component () {
+  const pathname = usePathname()
+
+  return (
+    <div className={'flex flex-col gap-1'}>
+      {
+        menu.map((item, index) => (
+          <Link href={item.path} key={index}
+                className={`text-sm mt-0 mx-4 px-4 py-1.5 font-semibold ${pathname.includes(item.path) ? 'bg-blue-100 text-blue-400' : 'hover:bg-blue-100 hover:text-blue-400'} rounded`}>
+            {item.name}
+          </Link>
+        ))
+      }
+    </div>
+  )
+}
--- app/components/Menu/index.tsx
+++ app/components/Menu/index.tsx
@@ -1,66 +1,26 @@
-'use client';
-
-import Link from "next/link";
-import {usePathname} from "next/navigation";
-import {useState} from "react";
-
-const menu = [
-  {
-    name: "Persona",
-    path: "/persona",
-    children: [],
-  },
-  {
-    name: "Discuss",
-    path: "/discuss",
-    children: [],
-  },
-  {
-    name: "Whisper",
-    path: "/whisper",
-    children: [],
-  },
-  {
-    name: "Activity",
-    path: "/activity",
-    children: [],
-  },
-  {
-    name: "Settings",
-    path: "/settings",
-    children: [],
-  },
-  {
-    name: "Help & Support",
-    path: "/help",
-    children: [],
-  },
-]
-
+import Image from 'next/image'
+import MenuList from './MenuList'
 
 export default function Component() {
-  const pathname = usePathname()
 
   return (
-    <div className={'flex flex-col h-full w-[260px] bg-gray-100 p-4 text-gray-500 gap-6'}>
-      <div className={'flex gap-4 p-2'}>
+    <div className={'flex flex-col h-full w-[260px] bg-gray-100 text-gray-500 gap-6 relative'}>
+      <div className={'flex gap-2 mt-5 mx-3 p-2.5'}>
         <div className={'bg-white h-8 w-8 rounded-full overflow-hidden truncate'}>
           Avatar
         </div>
         <div>
-          <div className={'text-sm'}>Tom</div>
+          <div className={'text-sm font-semibold'}>Tom</div>
           <div className={'text-xs'}>Estimated costs: $0.00</div>
         </div>
       </div>
-      <div className={'flex flex-col gap-1'}>
-        {
-          menu.map((item, index) => (
-            <Link href={item.path} key={index}
-                  className={`text-sm px-4 py-2 ${pathname.includes(item.path) ? 'bg-blue-100 text-blue-400' : 'hover:bg-blue-100 hover:text-blue-400'} rounded`}>
-              {item.name}
-            </Link>
-          ))
-        }
+      <MenuList />
+      <div className={'absolute bottom-0 w-full pl-6 pb-6 pt-6 pr-4'}>
+        <div className={'flex items-center gap-3 w-full'}>
+          <Image src={'/favicon.svg'} alt={''} width={'32'} height={'32'}/>
+          <div className={'text-xs'}>|</div>
+          <div className={'text-xs'}>Abandon Inc.</div>
+        </div>
       </div>
     </div>
   )
