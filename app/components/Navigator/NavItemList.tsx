06876d84b11b3e2d21f41042a88c1ec784d111bd
tunogya <tunogya@qq.com>
2023-09-03 03:04:12 +0800

    update

--- app/components/Navigator/NavItemList.tsx
+++ app/components/Navigator/NavItemList.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from "next/link";
 import {usePathname} from "next/navigation";
+import {useState} from "react";
 
 const menu = [
   {
@@ -37,13 +38,14 @@ const menu = [
 
 export default function Component () {
   const pathname = usePathname()
+  const [curPath, setCurPath] = useState(pathname)
 
   return (
     <div className={'flex flex-col gap-1'}>
       {
         menu.map((item, index) => (
-          <Link href={item.path} key={index}
-                className={`text-sm mt-0 mx-4 px-4 py-1.5 font-semibold ${pathname.includes(item.path) ? 'bg-blue-100 text-blue-400' : 'hover:bg-gray-200 hover:text-gray-500'} rounded`}>
+          <Link href={item.path} key={index} onClick={() => setCurPath(item.path)}
+                className={`text-sm mt-0 mx-4 px-4 py-1.5 font-semibold ${curPath.includes(item.path) ? 'bg-blue-100 text-blue-400' : 'hover:bg-gray-200 hover:text-gray-500'} rounded`}>
             {item.name}
           </Link>
         ))
