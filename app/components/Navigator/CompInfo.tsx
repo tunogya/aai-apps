9ae27f490946907eceacb4b8bc1ca2626a9d65e2
tunogya <tunogya@qq.com>
2023-09-03 02:41:20 +0800

    update

--- app/components/Navigator/CompInfo.tsx
+++ app/components/Navigator/CompInfo.tsx
@@ -4,7 +4,7 @@ import {Popover} from "@headlessui/react";
 const CompInfo = () => {
   return (
     <Popover className="relative w-full">
-      <Popover.Button className={'text-xs px-3 py-2 hover:bg-gray-200 rounded w-full text-start'}> ©{new Date().getFullYear()}, ABANDON INC.</Popover.Button>
+      <Popover.Button className={'text-xs p-2 hover:bg-gray-200 rounded w-full text-start'}> ©{new Date().getFullYear()}, ABANDON INC.</Popover.Button>
       <Popover.Panel className="absolute z-10 bottom-10 w-full border">
         <div className="bg-white p-2 rounded text-xs">
           8 The Green, STE R.
