38d533eb8d1ed01fc7d0471ff11e3167864d634f
tunogya <tunogya@qq.com>
2023-09-03 01:46:33 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,19 +46,6 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
                 gtag('config', 'G-HT9Q8GW970');
               `}
       </Script>
-      <Script id={'sw'}>
-        {`
-                if ('serviceWorker' in navigator) {
-                  window.addEventListener('load', function() {
-                    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                    }, function(err) {
-                      console.log('ServiceWorker registration failed: ', err);
-                    });
-                  });
-                }
-        `}
-      </Script>
       <TailwindIndicator/>
       {children}
       </body>
