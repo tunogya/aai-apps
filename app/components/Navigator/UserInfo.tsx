7e0ce10b116d5494019c397f73ca960cc37c34c9
tunogya <tunogya@qq.com>
2023-09-03 03:01:01 +0800

    update

--- app/components/Navigator/UserInfo.tsx
+++ app/components/Navigator/UserInfo.tsx
@@ -8,12 +8,22 @@ const userInfo = () => {
 
   return (
     <div className={'flex gap-2 mt-5 mx-3 p-2.5'}>
-      <div className={'bg-white h-8 w-8 rounded-full overflow-hidden'}>
+      <div className={'rounded-full min-w-[32px] min-h-[32px] max-w-[32px] max-h-[32px] overflow-hidden'}>
         <Image src={user?.picture!} height={32} width={32} alt={''} />
       </div>
-      <div>
+      <div className={'w-full group relative'}>
         <div className={'text-sm font-semibold'}>{user?.name ?? 'myself'}</div>
         <div className={'text-xs'}>Estimated costs: $0.00</div>
+        <a href={'/api/auth/logout'}
+          className={'absolute top-0 right-0 hidden group-hover:block cursor-pointer'}>
+          <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24"
+               strokeLinecap="round" strokeLinejoin="round" className="h-4 w-4" height="1em" width="1em"
+               xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+            <polyline points="16 17 21 12 16 7"></polyline>
+            <line x1="21" y1="12" x2="9" y2="12"></line>
+          </svg>
+        </a>
       </div>
     </div>
   )
