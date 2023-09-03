582d5cd7b4493fdd66899e193eddc537ba532530
tunogya <tunogya@qq.com>
2023-09-03 13:07:48 +0800

    update

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -3,7 +3,18 @@ import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0/edge";
 export default withPageAuthRequired(
   async function SSRPage() {
     const session = getSession();
-    return <div>help</div>;
+    return (
+      <div className={"flex flex-col gap-2 h-full w-full"}>
+        <div className={"flex pb-3 border-b items-center justify-between"}>
+          <div className={"text-xl font-semibold text-gray-800 py-2"}>
+            Help & Support
+          </div>
+          <div className={"flex space-x-3 text-sm py-2"}>
+            <button className={"px-3 h-8 border rounded"}>Search</button>
+          </div>
+        </div>
+      </div>
+    );
   },
   { returnTo: "/help" },
 );
