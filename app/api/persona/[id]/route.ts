e95371d7be98980d54581d4b97100b0169b71fda
tunogya <tunogya@qq.com>
2023-09-06 01:41:21 +0800

    update

--- app/api/persona/[id]/route.ts
+++ app/api/persona/[id]/route.ts
@@ -1,7 +1,7 @@
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import { NextRequest, NextResponse } from "next/server";
 import ddbDocClient from "@/utils/ddbDocClient";
-import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DeleteCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const GET = withApiAuthRequired(async (req: NextRequest, { params }) => {
   const session = await getSession();
@@ -31,16 +31,38 @@ const GET = withApiAuthRequired(async (req: NextRequest, { params }) => {
   }
 });
 
-const PATCH = withApiAuthRequired(async (req: NextRequest) => {
+const PATCH = withApiAuthRequired(async (req: NextRequest, { params }) => {
   return NextResponse.json({
     delete: true,
   });
 });
 
-const DELETE = withApiAuthRequired(async (req: NextRequest) => {
-  return NextResponse.json({
-    delete: true,
-  });
+const DELETE = withApiAuthRequired(async (req: NextRequest, { params }) => {
+  const session = await getSession();
+  const sub = session?.user.sub;
+  try {
+    await ddbDocClient.send(
+      new DeleteCommand({
+        TableName: "abandonai-dev",
+        Key: {
+          PK: sub,
+          SK: `PERSONA#${params?.id}`,
+        },
+      }),
+    );
+    return NextResponse.json({
+      delete: true,
+    });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        error: "something went wrong",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 });
 
 export { GET, PATCH, DELETE };
