d46999d652794f990400d23f2965ef4aa60cc54b
tunogya <tunogya@qq.com>
2023-09-06 01:37:27 +0800

    update

--- app/api/persona/[id]/route.ts
+++ app/api/persona/[id]/route.ts
@@ -1,11 +1,23 @@
-import { withApiAuthRequired } from "@auth0/nextjs-auth0/edge";
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import { NextRequest, NextResponse } from "next/server";
+import ddbDocClient from "@/utils/ddbDocClient";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
-export const runtime = "edge";
+const GET = withApiAuthRequired(async (req: NextRequest, { params }) => {
+  const session = await getSession();
+  const sub = session?.user.sub;
+  const id = params?.id;
+  // const data = await ddbDocClient.send(new GetCommand({
+  //   TableName: 'abandonai-dev',
+  //   Key: {
+  //     PK: sub,
+  //     SK: `PERSONA#`
+  //   }
+  // }))
 
-const GET = withApiAuthRequired(async (req: NextRequest) => {
   return NextResponse.json({
-    delete: true,
+    sub: sub,
+    p_id: id,
   });
 });
 
