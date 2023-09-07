280173db676b85a4fa79f8f775a67a6a1a5ecad8
tunogya <tunogya@qq.com>
2023-09-08 01:05:26 +0800

    update

--- components/RecentNav.tsx
+++ components/RecentNav.tsx
@@ -1,9 +1,9 @@
 const RecentNav = () => {
   return (
     <div className={"ml-6 space-y-2"}>
-      <div className={"text-xs text-gray-800"}>Recent</div>
-      <div className={"text-sm font-light"}>Dashboard</div>
-      <div className={"text-sm font-light"}>Chat</div>
+      <div className={"text-xs text-gray-800 font-semibold"}>Recent</div>
+      <div className={"text-sm text-gray-500"}>Dashboard</div>
+      <div className={"text-sm text-gray-500"}>Chat</div>
     </div>
   );
 };
