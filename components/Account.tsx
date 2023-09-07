5abd840fb6c3ce0d0c4d0429482d0f8b149ed483
tunogya <tunogya@qq.com>
2023-09-08 00:03:16 +0800

    update

--- components/Account.tsx
+++ components/Account.tsx
@@ -1,5 +1,27 @@
-const Account = () => {
-  return <div className={"ml-6"}>account</div>;
+import { getSession } from "@auth0/nextjs-auth0/edge";
+import Image from "next/image";
+
+export const runtime = "edge";
+
+const Account = async () => {
+  const session = await getSession();
+  return (
+    <div
+      className={
+        "ml-4 flex items-center space-x-2 hover:bg-gray-100 px-2 py-1 rounded cursor-pointer"
+      }
+    >
+      <Image src={session?.user.picture} width={30} height={30} alt={""} />
+      <div className={"truncate"}>
+        <div className={"text-sm text-gray-800 truncate"}>
+          {session?.user.email}
+        </div>
+        <div className={"text-xs text-gray-500 font-light truncate"}>
+          Estimate cost: $0
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
