23717fb5468bdf314c9d5da987b08574f93cf9cc
tunogya <tunogya@qq.com>
2023-09-12 00:27:41 +0800

    update

--- app/chat/Chat.tsx
+++ app/chat/Chat.tsx
@@ -49,7 +49,9 @@ export default function Chat() {
             />
             <button
               type="submit"
-              className={`h-6 w-6 flex items-center text-purple-600 justify-center rounded ${
+              className={`h-6 w-6 flex items-center ${
+                isLoading ? "animate-pulse" : ""
+              } text-stone-800 justify-center rounded ${
                 isLoading ? "cursor-wait" : "cursor-pointer"
               }`}
             >
