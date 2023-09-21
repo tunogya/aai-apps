e4b4d4c0c2fc9f316571c6d7d3bad65e615001dd
tunogya <tunogya@qq.com>
2023-09-21 23:30:27 +0800

    update

--- app/api/dashboard/today/route.ts
+++ app/api/dashboard/today/route.ts
@@ -1,21 +1,76 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "@auth0/nextjs-auth0";
+import ddbDocClient from "@/utils/ddbDocClient";
+import { GetCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 const GET = async (req: NextRequest) => {
   const session = await getSession();
   const sub = session?.user.sub;
 
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+
+  const firstDay = new Date(year, month, 1);
+  const numDays = new Date(year, month + 1, 0).getDate();
+
+  const dates = [];
+  for (let i = 0; i < numDays; i++) {
+    const date = new Date(year, month, i + 1, 8, 0, 0);
+    if (date <= today) {
+      dates.push(date.toISOString().slice(0, 10));
+    }
+  }
+
+  const { Items: UsageItems } = await ddbDocClient.send(
+    new QueryCommand({
+      TableName: "abandonai-prod",
+      KeyConditionExpression: "#pk = :pk AND begins_with(#sk, :sk)",
+      FilterExpression: `#created >= :firstDay`,
+      ExpressionAttributeNames: {
+        "#pk": "PK",
+        "#sk": "SK",
+        "#created": "created",
+      },
+      ExpressionAttributeValues: {
+        ":pk": `USER#${sub}`,
+        ":sk": "USAGE#",
+        ":firstDay": Math.floor(firstDay.getTime() / 1000),
+      },
+      ProjectionExpression: "total_cost, created",
+    }),
+  );
+
+  const { Item: BalanceItem } = await ddbDocClient.send(
+    new GetCommand({
+      TableName: "abandonai-prod",
+      Key: {
+        PK: `USER#${sub}`,
+        SK: `BALANCE`,
+      },
+    }),
+  );
+
+  const charts = dates.map((item) => ({
+    date: item,
+    total_cost: UsageItems?.filter(
+      (usageItem) =>
+        new Date(usageItem.created * 1000).toISOString().slice(0, 10) === item,
+    ).reduce((acc, usageItem) => acc + usageItem.total_cost, 0),
+  }));
+
   return NextResponse.json({
+    charts: charts,
     cost: {
-      today: 10,
-      yesterday: 5,
+      today: charts[charts.length - 1].total_cost,
+      yesterday: charts[charts.length - 2].total_cost,
     },
     estimate: {
       month: 20,
     },
     advance_pay: {
-      balance: 100,
-      status: "NORMAL",
+      balance: BalanceItem?.balance || 0,
+      status: BalanceItem?.balance >= 0 ? "NORMAL" : "LOCKED",
     },
   });
 };
--- app/dashboard/Today.tsx
+++ app/dashboard/Today.tsx
@@ -1,6 +1,17 @@
 "use client";
 import React from "react";
 import useSWR from "swr";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const CSR = () => {
   const { data, isLoading } = useSWR("/api/dashboard/today", (url) =>
@@ -14,32 +25,68 @@ const CSR = () => {
           <div className={"w-64 space-y-1"}>
             <div className={"whitespace-nowrap"}>Cost & usage</div>
             <div className={"text-stone-800 text-xl"}>
-              US${data?.cost?.today || "-"}
+              US$
+              {data?.cost?.today?.toLocaleString("en-US", {
+                maximumFractionDigits: 6,
+                minimumFractionDigits: 2,
+              }) || 0}
             </div>
           </div>
           <div className={"w-64 space-y-1"}>
             <div className={"whitespace-nowrap"}>Yesterday</div>
             <div className={"text-stone-400 text-md"}>
-              US${data?.cost?.yesterday || "-"}
+              US$
+              {data?.cost?.yesterday?.toLocaleString("en-US", {
+                maximumFractionDigits: 6,
+                minimumFractionDigits: 2,
+              }) || 0}
             </div>
           </div>
         </div>
-        <div></div>
+        <div className={"h-full w-full mt-4"}>
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart data={data?.charts || []}>
+              <Tooltip />
+              <XAxis
+                dataKey="date"
+                axisLine={false}
+                tickLine={false}
+                tick={{ fontSize: "12px" }}
+                tickFormatter={(value, index) => value.slice(-4)}
+              />
+              <Bar dataKey="total_cost" fill="#8884d8" minPointSize={1} />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
       </div>
       <div className={"flex-1 h-full text-sm text-stone-800"}>
         <div className={"h-[50%]"}>
           <div className={"space-y-1"}>
             <div className={"whitespace-nowrap"}>Estimate cost this month</div>
             <div className={"text-stone-800 text-xl"}>
-              US${data?.estimate?.month || "-"}
+              US$
+              {data?.estimate?.month?.toLocaleString("en-US", {
+                maximumFractionDigits: 6,
+                minimumFractionDigits: 2,
+              }) || 0}
             </div>
           </div>
         </div>
         <div className={"h-[50%]"}>
           <div className={"space-y-1"}>
             <div className={"whitespace-nowrap"}>Advance pay balance</div>
-            <div className={"text-stone-800 text-xl"}>
-              US${data?.advance_pay?.balance || "-"}
+            <div
+              className={`${
+                data?.advance_pay?.balance < 0
+                  ? "text-red-500"
+                  : "text-stone-800"
+              }  text-xl`}
+            >
+              US$
+              {data?.advance_pay?.balance?.toLocaleString("en-US", {
+                maximumFractionDigits: 6,
+                minimumFractionDigits: 2,
+              }) || 0}
             </div>
           </div>
         </div>
