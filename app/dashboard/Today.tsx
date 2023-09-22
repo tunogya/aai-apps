f5c59ba22fac06ddfb6d5e6ca0833d0086dd7ca5
tunogya <tunogya@qq.com>
2023-09-22 21:57:33 +0800

    update

--- app/dashboard/Today.tsx
+++ app/dashboard/Today.tsx
@@ -12,15 +12,15 @@ const CSR = () => {
   return (
     <div className={"flex flex-col xl:flex-row gap-10 xl:gap-20 h-fit w-full"}>
       <div className={"w-full xl:max-w-2xl h-full text-sm text-stone-800"}>
-        <div className={"flex w-full"}>
-          <div className={"w-[50%] space-y-1"}>
+        <div className={"flex w-full gap-10"}>
+          <div className={"w-[200px] space-y-1"}>
             <div className={"whitespace-nowrap"}>Cost & usage</div>
             <div className={"text-stone-800 text-xl"}>
               US$
               {roundUp(data?.cost?.today || 0, 6)}
             </div>
           </div>
-          <div className={"w-[50%] space-y-1"}>
+          <div className={"w-[200px] space-y-1"}>
             <div className={"whitespace-nowrap"}>Yesterday</div>
             <div className={"text-stone-400 text-md"}>
               US$
@@ -49,14 +49,14 @@ const CSR = () => {
           "flex flex-row xl:flex-col w-96 h-full text-sm text-stone-800 gap-20 xl:gap-10"
         }
       >
-        <div className={"space-y-1"}>
+        <div className={"w-[200px] space-y-1"}>
           <div className={"whitespace-nowrap"}>Estimate cost this month</div>
           <div className={"text-stone-800 text-xl"}>
             US$
             {roundUp(data?.estimate?.month || 0, 6)}
           </div>
         </div>
-        <div className={"space-y-1"}>
+        <div className={"w-[200px] space-y-1"}>
           <div className={"whitespace-nowrap"}>Advance pay balance</div>
           <div
             className={`${
