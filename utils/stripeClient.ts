67cce07419d15b9c91142a107215bf6edc475c05
tunogya <tunogya@qq.com>
2023-09-24 20:49:36 +0800

    update

--- utils/stripeClient.ts
+++ utils/stripeClient.ts
@@ -1,7 +1,8 @@
+import "server-only";
 import Stripe from "stripe";
 
-const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-08-16",
+const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2022-11-15",
 });
 
 export default stripeClient;
