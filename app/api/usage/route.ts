32307504d7ebcd1df345e19c4db8b85fb367bf77
tunogya <tunogya@qq.com>
2023-09-26 00:09:11 +0800

    update

--- app/api/usage/route.ts
+++ app/api/usage/route.ts
@@ -2,23 +2,12 @@ import { getSession } from "@auth0/nextjs-auth0";
 import { NextRequest, NextResponse } from "next/server";
 import ddbDocClient from "@/utils/ddbDocClient";
 import { QueryCommand } from "@aws-sdk/lib-dynamodb";
-import redisClient from "@/utils/redisClient";
 
 const GET = async (req: NextRequest) => {
   const session = await getSession();
   const sub = session?.user.sub;
   const limit = Number(req?.nextUrl?.searchParams?.get("limit") || 20);
-  const page = Number(req?.nextUrl?.searchParams?.get("page") || 0);
-  let exclusiveStartKey = undefined;
-  if (page >= 1) {
-    exclusiveStartKey = await redisClient.get(`${sub}#UASGE#P${page}`);
-    if (!exclusiveStartKey) {
-      return NextResponse.json({
-        items: [],
-        count: 0,
-      });
-    }
-  }
+  const cursor = req?.nextUrl?.searchParams?.get("cursor") || undefined;
   const { Items, Count, LastEvaluatedKey } = await ddbDocClient.send(
     new QueryCommand({
       TableName: "abandonai-prod",
@@ -35,17 +24,18 @@ const GET = async (req: NextRequest) => {
       ScanIndexForward: false,
       ProjectionExpression:
         "model, prompt_tokens, completion_tokens, total_cost, created",
-      ExclusiveStartKey: exclusiveStartKey,
+      ExclusiveStartKey: cursor
+        ? {
+            PK: `USER#${sub}`,
+            SK: `USAGE#${cursor}`,
+          }
+        : undefined,
     }),
   );
-  if (LastEvaluatedKey) {
-    await redisClient.set(`${sub}#UASGE#P${page + 1}`, LastEvaluatedKey, {
-      ex: 60 * 60 * 24,
-    });
-  }
   return NextResponse.json({
     items: Items,
     count: Count,
+    nextCursor: LastEvaluatedKey?.SK.replace("USAGE#", "") || undefined,
   });
 };
 
--- app/usage/Pages.tsx
+++ app/usage/Pages.tsx
@@ -1,19 +1,19 @@
 "use client";
-import useSWR from "swr";
 import { roundUp } from "@/utils/roundUp";
 import moment from "moment";
-import React, { FC, useMemo, useState } from "react";
+import React from "react";
+import useSWRInfinite from "swr/infinite";
 
 const Pages = () => {
-  const [cnt, setCnt] = useState(1);
+  const getKey = (pageIndex: number, previousPageData: any) => {
+    if (previousPageData && !previousPageData.nextCursor) return null;
+    if (pageIndex === 0) return `/api/usage?limit=20`;
+    return `/api/usage?cursor=${previousPageData.nextCursor}&limit=20`;
+  };
 
-  const pages = useMemo(() => {
-    let pages = [];
-    for (let i = 0; i < cnt; i++) {
-      pages.push(<Page page={i} key={i} />);
-    }
-    return pages;
-  }, [cnt]);
+  const { data, isLoading, size, setSize } = useSWRInfinite(getKey, (url) =>
+    fetch(url).then((res) => res.json()),
+  );
 
   return (
     <div className={"w-full overflow-hidden"}>
@@ -57,54 +57,44 @@ const Pages = () => {
             </th>
           </tr>
         </thead>
-        <tbody>{pages}</tbody>
+        <tbody>
+          {data &&
+            data
+              .reduce((a, b) => a.concat(b?.items || []), [])
+              .map((item: any, index: number) => (
+                <tr key={index} className={"text-xs text-gray-600 border-b"}>
+                  <td
+                    className={
+                      "font-semibold pt-2 pr-4 pb-2 overflow-x-hidden text-black"
+                    }
+                  >
+                    {item.model}
+                  </td>
+                  <td className={"pt-2 pr-4 pb-2 overflow-x-hidden "}>
+                    {item.prompt_tokens}
+                  </td>
+                  <td className={"pt-2 pr-4 pb-2 overflow-x-hidden"}>
+                    {item.completion_tokens}
+                  </td>
+                  <td className={"pt-2 pr-4 pb-2 overflow-x-hidden"}>
+                    {roundUp(item.total_cost, 6)}
+                  </td>
+                  <td className={"pt-2 pr-4 pb-2 overflow-x-hidden"}>
+                    {moment(item.created * 1000)
+                      .startOf("hour")
+                      .fromNow()}
+                  </td>
+                </tr>
+              ))}
+        </tbody>
       </table>
       <button
         className={"w-full border p-2 mt-4 text-xs hover:bg-gray-50 rounded"}
-        onClick={() => setCnt(cnt + 1)}
+        onClick={() => setSize(size + 1)}
       >
-        Load More
+        {isLoading ? "Loading..." : "Load More"}
       </button>
     </div>
-  );
-};
-
-const Page: FC<{
-  page: number;
-}> = ({ page }) => {
-  const { data } = useSWR(`/api/usage?page=${page}`, (url) =>
-    fetch(url).then((res) => res.json()),
-  );
-
-  return (
-    <>
-      {data?.items &&
-        data.items.map((item: any, index: number) => (
-          <tr key={index} className={"text-xs text-gray-600 border-b"}>
-            <td
-              className={
-                "font-semibold pt-2 pr-4 pb-2 overflow-x-hidden text-black"
-              }
-            >
-              {item.model}
-            </td>
-            <td className={"pt-2 pr-4 pb-2 overflow-x-hidden "}>
-              {item.prompt_tokens}
-            </td>
-            <td className={"pt-2 pr-4 pb-2 overflow-x-hidden"}>
-              {item.completion_tokens}
-            </td>
-            <td className={"pt-2 pr-4 pb-2 overflow-x-hidden"}>
-              {roundUp(item.total_cost, 6)}
-            </td>
-            <td className={"pt-2 pr-4 pb-2 overflow-x-hidden"}>
-              {moment(item.created * 1000)
-                .startOf("hour")
-                .fromNow()}
-            </td>
-          </tr>
-        ))}
-    </>
   );
 };
 
