140e005018038d8f7b0bd727b3bdd4daadf387b0
tunogya <tunogya@qq.com>
2023-09-26 11:19:03 +0800

    update

--- utils/aiModels.ts
+++ utils/aiModels.ts
@@ -2,65 +2,65 @@ export const AI_MODELS_MAP = new Map([
   [
     "gpt-4",
     {
-      model: "8K context",
-      input: 0.03,
-      output: 0.06,
+      content: 8192,
+      input_price: 0.03,
+      output_price: 0.06,
     },
   ],
   [
     "gpt-4-0314",
     {
-      model: "8K context",
-      input: 0.03,
-      output: 0.06,
+      content: 8192,
+      input_price: 0.03,
+      output_price: 0.06,
     },
   ],
   [
     "gpt-4-0613",
     {
-      model: "8K context",
-      input: 0.03,
-      output: 0.06,
+      content: 8192,
+      input_price: 0.03,
+      output_price: 0.06,
     },
   ],
   [
     "gpt-3.5-turbo-0613",
     {
-      model: "4K context",
-      input: 0.0015,
-      output: 0.0002,
+      content: 4092,
+      input_price: 0.0015,
+      output_price: 0.0002,
     },
   ],
   [
     "gpt-3.5-turbo-0301",
     {
-      model: "4K context",
-      input: 0.0015,
-      output: 0.0002,
+      content: 4092,
+      input_price: 0.0015,
+      output_price: 0.0002,
     },
   ],
   [
     "gpt-3.5-turbo",
     {
-      model: "4K context",
-      input: 0.0015,
-      output: 0.0002,
+      content: 4092,
+      input_price: 0.0015,
+      output_price: 0.0002,
     },
   ],
   [
     "gpt-3.5-turbo-16k",
     {
-      model: "16K context",
-      input: 0.003,
-      output: 0.004,
+      content: 16368,
+      input_price: 0.003,
+      output_price: 0.004,
     },
   ],
   [
     "gpt-3.5-turbo-16k-0613",
     {
-      model: "16K context",
-      input: 0.003,
-      output: 0.004,
+      content: 16368,
+      input_price: 0.003,
+      output_price: 0.004,
     },
   ],
 ]);
