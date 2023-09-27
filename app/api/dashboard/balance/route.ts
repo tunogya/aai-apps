30ed2f9eb2fedefd932da8f0bcc6aa385f3a60eb
tunogya <20251942+tunogya@users.noreply.github.com>
2023-09-27 15:08:37 +0800

    get balance from redis

--- app/api/dashboard/balance/route.ts
+++ app/api/dashboard/balance/route.ts
@@ -1,23 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "@auth0/nextjs-auth0";
-import ddbDocClient from "@/utils/ddbDocClient";
-import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import redisClient from "@/utils/redisClient";
 
 const GET = async (req: NextRequest) => {
   const session = await getSession();
   const sub = session?.user.sub;
   try {
-    const { Item } = await ddbDocClient.send(
-      new GetCommand({
-        TableName: "abandonai-prod",
-        Key: {
-          PK: `USER#${sub}`,
-          SK: `BALANCE`,
-        },
-      }),
-    );
+    const balance = (await redisClient.get(`${sub}:balance`)) || 0;
     return NextResponse.json({
-      balance: Item?.balance || 0,
+      balance: balance,
     });
   } catch (e) {
     return NextResponse.json(
