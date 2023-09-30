b4f20ece961cc52d6128f3a0e476d3f3bcdc7b22
tunogya <20251942+tunogya@users.noreply.github.com>
2023-09-30 22:53:18 +0800

    update

--- app/dashboard/Today.tsx
+++ app/dashboard/Today.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import useSWR from "swr";
 import {
   XAxis,
@@ -24,17 +24,25 @@ const CSR = () => {
   );
 
   return (
-    <div className={"flex flex-col xl:flex-row gap-10 h-fit w-full"}>
+    <div className={"flex flex-col xl:flex-row gap-4 md:gap-10 h-fit w-full"}>
       <div className={"w-full xl:max-w-3xl h-full text-sm text-black"}>
-        <div className={"flex w-full gap-10"}>
-          <div className={"w-[200px] space-y-1"}>
+        <div className={"flex w-full flex-col md:flex-row gap-3"}>
+          <div
+            className={
+              "w-full md:w-[200px] space-y-1 border p-3 md:border-none md:p-0 rounded"
+            }
+          >
             <div className={"whitespace-nowrap"}>Cost & usage</div>
             <div className={"text-black text-xl"}>
               US$
               {roundUp(data?.cost?.today || 0, 6)}
             </div>
           </div>
-          <div className={"w-[200px] space-y-1"}>
+          <div
+            className={
+              "w-full md:w-[200px] space-y-1 border p-3 md:border-none md:p-0 rounded"
+            }
+          >
             <div className={"whitespace-nowrap"}>Yesterday</div>
             <div className={"text-gray-400 text-md"}>
               US$
@@ -42,7 +50,7 @@ const CSR = () => {
             </div>
           </div>
         </div>
-        <div className={"h-[128px] w-full mt-4"}>
+        <div className={"h-[256px] md:h-[128px] w-full mt-4"}>
           <ResponsiveContainer width="100%" height="100%">
             <BarChart data={data?.daily || []}>
               <CartesianGrid strokeOpacity={0.5} horizontal={false} />
@@ -115,17 +123,25 @@ const CSR = () => {
       </div>
       <div
         className={
-          "flex flex-row xl:flex-col w-96 h-full text-sm text-black gap-20 xl:gap-10"
+          "flex flex-col md:flex-row xl:flex-col w-full md:w-96 h-full text-sm text-black gap-3 xl:gap-10"
         }
       >
-        <div className={"w-[200px] space-y-1"}>
+        <div
+          className={
+            "w-full md:w-[200px] space-y-1 border p-3 md:border-none md:p-0 rounded"
+          }
+        >
           <div className={"whitespace-nowrap"}>Total cost this month</div>
           <div className={"text-black text-xl"}>
             US$
             {roundUp(data?.cost?.month || 0, 6)}
           </div>
         </div>
-        <div className={"w-[200px] space-y-1"}>
+        <div
+          className={
+            "w-full md:w-[200px] space-y-1 border p-3 md:border-none md:p-0 rounded"
+          }
+        >
           <div className={"whitespace-nowrap"}>Advance pay balance</div>
           <div
             className={`${
@@ -136,7 +152,7 @@ const CSR = () => {
             {roundUp(Math.abs(balanceData?.balance) || 0, 6)}
           </div>
           <DepositButton
-            className={`text-sm px-3 py-1 rounded text-white bg-[#0066FF] font-semibold flex items-center gap-1`}
+            className={`text-sm py-2 px-3 md:py-1 rounded text-white bg-[#0066FF] font-semibold flex items-center gap-1 w-full md:w-fit`}
           />
         </div>
       </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,36 +1,25 @@
 import React from "react";
-import { getSession } from "@auth0/nextjs-auth0/edge";
 import Today from "@/app/dashboard/Today";
-import Overview from "@/app/dashboard/Overview";
 
 export const runtime = "edge";
 
 export default async function SSRPage() {
-  const session = await getSession();
   return (
     <div
       className={
         "px-4 md:px-10 pt-4 absolute h-[calc(100vh-60px)] w-full overflow-y-auto pb-40 space-y-20"
       }
     >
-      <div className={"space-y-8"}>
+      <div className={"space-y-3 md:space-y-8"}>
         <div
-          className={"text-2xl font-semibold text-black border-b w-full py-2"}
+          className={
+            "text-2xl font-semibold text-black md:border-b w-full py-2"
+          }
         >
           Today
         </div>
         <Today />
       </div>
-      {/*<div className={"space-y-8"}>*/}
-      {/*  <div*/}
-      {/*    className={*/}
-      {/*      "text-2xl font-semibold text-black border-b w-full py-2"*/}
-      {/*    }*/}
-      {/*  >*/}
-      {/*    Your overview*/}
-      {/*  </div>*/}
-      {/*  <Overview />*/}
-      {/*</div>*/}
     </div>
   );
 }
