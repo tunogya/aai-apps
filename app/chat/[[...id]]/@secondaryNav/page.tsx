8bce6785f601270a75d548e449e6fbf9a7f52f5b
tunogya <20251942+tunogya@users.noreply.github.com>
2023-10-02 18:52:51 +0800

    update

--- app/chat/[[...id]]/@secondaryNav/page.tsx
+++ app/chat/[[...id]]/@secondaryNav/page.tsx
@@ -79,80 +79,85 @@ const SecondaryNav = () => {
         <div className={"text-sm"}>New Chat</div>
       </Link>
       <div className={"h-full overflow-y-auto px-2"}>
-        {reducedData
-          .filter((item: any) => !deleteItems.includes(item.SK))
-          .sort((a: any, b: any) => b.updated - a.updated)
-          .map((item: any) => (
-            <div
-              key={item.SK}
-              className={`relative flex group items-center gap-2 ${
-                item.SK.replace("CHAT2#", "") === currentChatId
-                  ? "bg-gray-100"
-                  : ""
-              } hover:bg-gray-100 rounded px-3 py-2 cursor-pointer select-none`}
-            >
-              <Link
-                href={`/chat/${item.SK.replace("CHAT2#", "")}?model=${
-                  searchParams.get("model") || "GPT-3.5"
-                }`}
-                prefetch
-                className={`flex w-full items-center`}
-              >
-                <div className={"w-6 shrink-0"}>
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-4 w-4"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                  </svg>
-                </div>
-                <div className={"truncate text-sm mr-4"}>{item.title}</div>
-              </Link>
-              <button
-                className={
-                  "absolute right-2 hidden group-hover:flex text-black hover:text-red-500"
-                }
-                onClick={async () => {
-                  const _newDeleteItems = [...deleteItems, item.SK];
-                  setDeleteItems(_newDeleteItems);
-                  sessionStorage.setItem(
-                    "deleteItems",
-                    JSON.stringify(_newDeleteItems),
-                  );
-                  await deleteChat(item.SK.replace("CHAT2#", ""));
-                }}
-              >
-                <svg
-                  stroke="currentColor"
-                  fill="none"
-                  strokeWidth="2"
-                  viewBox="0 0 24 24"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="h-4 w-4"
-                  height="1em"
-                  width="1em"
-                  xmlns="http://www.w3.org/2000/svg"
+        {reducedData.filter((item: any) => !deleteItems.includes(item.SK))
+          .length > 0 && (
+          <div className={"mb-2"}>
+            {reducedData
+              .filter((item: any) => !deleteItems.includes(item.SK))
+              .sort((a: any, b: any) => b.updated - a.updated)
+              .map((item: any) => (
+                <div
+                  key={item.SK}
+                  className={`relative flex group items-center gap-2 ${
+                    item.SK.replace("CHAT2#", "") === currentChatId
+                      ? "bg-gray-100"
+                      : ""
+                  } hover:bg-gray-100 rounded px-3 py-2 cursor-pointer select-none`}
                 >
-                  <polyline points="3 6 5 6 21 6"></polyline>
-                  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                  <line x1="10" y1="11" x2="10" y2="17"></line>
-                  <line x1="14" y1="11" x2="14" y2="17"></line>
-                </svg>
-              </button>
-            </div>
-          ))}
+                  <Link
+                    href={`/chat/${item.SK.replace("CHAT2#", "")}?model=${
+                      searchParams.get("model") || "GPT-3.5"
+                    }`}
+                    prefetch
+                    className={`flex w-full items-center`}
+                  >
+                    <div className={"w-6 shrink-0"}>
+                      <svg
+                        stroke="currentColor"
+                        fill="none"
+                        strokeWidth="2"
+                        viewBox="0 0 24 24"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="h-4 w-4"
+                        height="1em"
+                        width="1em"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
+                      </svg>
+                    </div>
+                    <div className={"truncate text-sm mr-4"}>{item.title}</div>
+                  </Link>
+                  <button
+                    className={
+                      "absolute right-2 hidden group-hover:flex text-black hover:text-red-500"
+                    }
+                    onClick={async () => {
+                      const _newDeleteItems = [...deleteItems, item.SK];
+                      setDeleteItems(_newDeleteItems);
+                      sessionStorage.setItem(
+                        "deleteItems",
+                        JSON.stringify(_newDeleteItems),
+                      );
+                      await deleteChat(item.SK.replace("CHAT2#", ""));
+                    }}
+                  >
+                    <svg
+                      stroke="currentColor"
+                      fill="none"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="h-4 w-4"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <polyline points="3 6 5 6 21 6"></polyline>
+                      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                      <line x1="10" y1="11" x2="10" y2="17"></line>
+                      <line x1="14" y1="11" x2="14" y2="17"></line>
+                    </svg>
+                  </button>
+                </div>
+              ))}
+          </div>
+        )}
         {haveMore ? (
           <button
-            className={`w-full border p-2 mt-4 text-xs hover:bg-gray-50 rounded ${
+            className={`w-full border p-2 text-xs hover:bg-gray-50 rounded ${
               isValidating ? "cursor-wait" : ""
             }`}
             onClick={() => setSize(size + 1)}
@@ -162,7 +167,7 @@ const SecondaryNav = () => {
         ) : (
           <div
             className={
-              "w-full border p-2 mt-2 text-xs bg-gray-50 rounded text-center cursor-not-allowed"
+              "w-full border p-2 text-xs bg-gray-50 rounded text-center cursor-not-allowed"
             }
           >
             No more data.
