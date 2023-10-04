8f80d2b02da3dbf7dd55faa3189cb61797b4601b
tunogya <20251942+tunogya@users.noreply.github.com>
2023-10-04 13:59:39 +0800

    Revert "get balance from redis"
    This reverts commit 30ed2f9eb2fedefd932da8f0bcc6aa385f3a60eb.

--- app/api/dashboard/balance/route.ts
+++ app/api/dashboard/balance/route.ts
@@ -1,14 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "@auth0/nextjs-auth0";
-import redisClient from "@/utils/redisClient";
+import ddbDocClient from "@/utils/ddbDocClient";
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const GET = async (req: NextRequest) => {
   const session = await getSession();
   const sub = session?.user.sub;
   try {
-    const balance = (await redisClient.get(`${sub}:balance`)) || 0;
+    const { Item } = await ddbDocClient.send(
+      new GetCommand({
+        TableName: "abandonai-prod",
+        Key: {
+          PK: `USER#${sub}`,
+          SK: `BALANCE`,
+        },
+      }),
+    );
     return NextResponse.json({
-      balance: balance,
+      balance: Item?.balance || 0,
     });
   } catch (e) {
     return NextResponse.json(
