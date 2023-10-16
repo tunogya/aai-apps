75fc20da886bcc1213a0d11d7d077dd6263b9378
tunogya <20251942+tunogya@users.noreply.github.com>
2023-10-16 16:43:09 +0800

    update

--- app/dashboard/ApiKey.tsx
+++ app/dashboard/ApiKey.tsx
@@ -2,13 +2,19 @@
 
 import useSWR from "swr";
 import { useState } from "react";
-import { ArrowPathIcon, TrashIcon } from "@heroicons/react/24/solid";
+import {
+  ArrowPathIcon,
+  TrashIcon,
+  EyeIcon,
+  EyeSlashIcon,
+} from "@heroicons/react/24/solid";
 
 const ApiKey = () => {
   const { data, mutate, isLoading } = useSWR("/api/token", (url) =>
     fetch(url).then((res) => res.json()),
   );
   const [status, setStatus] = useState("idle");
+  const [show, setShow] = useState(false);
 
   const refreshApiKey = async () => {
     setStatus("loading");
@@ -49,9 +55,15 @@ const ApiKey = () => {
   };
 
   return (
-    <div className={"text-gray-800 flex flex-col gap-2"}>
+    <div className={"text-gray-800 flex flex-col"}>
       <div className={"text-2xl font-semibold border-b py-2"}>API Key</div>
-      <div className={"flex flex-col gap-4 justify-center"}>
+      <div className={"text-sm text-gray-500 max-w-3xl mt-2"}>
+        Do not share your API key with others, or expose it in the browser or
+        other client-side code. In order to protect the security of your
+        account, AbandonAI may also automatically disable any API key that we
+        have found has leaked publicly.
+      </div>
+      <div className={"flex flex-col gap-4 justify-center mt-4"}>
         {isLoading || status === "loading" ? (
           <div className={"p-2 bg-gray-100 w-fit rounded-lg min-w-[360px]"}>
             ...
@@ -62,10 +74,31 @@ const ApiKey = () => {
               "px-4 py-2 bg-gray-100 w-fit rounded-lg select-text min-w-[360px]"
             }
           >
-            {data?.token || "-"}
+            {show
+              ? data?.token || "-"
+              : data?.token?.slice(0, 7) + "..." + data.token.slice(-4)}
           </div>
         )}
         <div className={"flex gap-2 text-sm h-fit"}>
+          <button
+            className={
+              "px-2 py-1 rounded-lg border hover:bg-gray-500 hover:text-white hover:border-white font-semibold disabled:cursor-wait flex gap-1 items-center"
+            }
+            onClick={() => setShow(!show)}
+            disabled={status !== "idle"}
+          >
+            {show ? (
+              <>
+                <EyeSlashIcon className={"w-4 h-4"} />
+                Hidden
+              </>
+            ) : (
+              <>
+                <EyeIcon className={"w-4 h-4"} />
+                Show
+              </>
+            )}
+          </button>
           <button
             className={
               "px-2 py-1 rounded-lg border hover:bg-red-500 hover:text-white hover:border-white font-semibold disabled:cursor-wait flex gap-1 items-center"
