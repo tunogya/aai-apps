7960883bf84018c5027bcf49e6eb0465371194e9
tunogya <20251942+tunogya@users.noreply.github.com>
2023-10-20 14:01:20 +0800

    update

--- app/api/usage/route.ts
+++ app/api/usage/route.ts
@@ -2,12 +2,19 @@ import { getSession } from "@auth0/nextjs-auth0";
 import { NextRequest, NextResponse } from "next/server";
 import ddbDocClient from "@/utils/ddbDocClient";
 import { QueryCommand } from "@aws-sdk/lib-dynamodb";
+import redisClient from "@/utils/redisClient";
 
 const GET = async (req: NextRequest) => {
   const session = await getSession();
   const sub = session?.user.sub;
   const limit = Number(req?.nextUrl?.searchParams?.get("limit") || 20);
   const cursor = req?.nextUrl?.searchParams?.get("cursor") || undefined;
+
+  const cache = await redisClient.get(`usage:${sub}:${limit}:${cursor}`);
+  if (cache) {
+    return NextResponse.json(cache);
+  }
+
   const { Items, Count, LastEvaluatedKey } = await ddbDocClient.send(
     new QueryCommand({
       TableName: "abandonai-prod",
@@ -32,11 +39,22 @@ const GET = async (req: NextRequest) => {
         : undefined,
     }),
   );
-  return NextResponse.json({
+
+  const data = {
     items: Items,
     count: Count,
     nextCursor: LastEvaluatedKey?.SK.replace("USAGE#", "") || undefined,
-  });
+  };
+
+  await redisClient.set(
+    `usage:${sub}:${limit}:${cursor}`,
+    JSON.stringify(data),
+    {
+      ex: 60,
+    },
+  );
+
+  return NextResponse.json(data);
 };
 
 export { GET };
