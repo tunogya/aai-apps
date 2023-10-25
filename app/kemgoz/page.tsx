928b1389db5d32b5a235931ea7114323d0d499af
tunogya <20251942+tunogya@users.noreply.github.com>
2023-10-26 00:05:06 +0800

    update

--- app/kemgoz/page.tsx
+++ app/kemgoz/page.tsx
@@ -1,7 +1,10 @@
 "use client";
-import Eye1 from "@/app/kemgoz/Eye1";
 import { XMarkIcon } from "@heroicons/react/20/solid";
 import { useRouter } from "next/navigation";
+import React, { useEffect, useLayoutEffect, useMemo, useRef } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { AsciiEffect } from "three/examples/jsm/effects/AsciiEffect";
+import { OrbitControls } from "@react-three/drei";
 
 export default function CSRPage() {
   const router = useRouter();
@@ -10,7 +13,7 @@ export default function CSRPage() {
     <>
       <button
         className={
-          "absolute text-black top-4 right-4 z-50 p-2 bg-gray-500 hover:bg-gray-200 rounded-full"
+          "absolute text-black top-4 right-4 z-50 p-2 hover:bg-gray-100 bg-gray-200 rounded-full"
         }
         onClick={() => {
           router.back();
@@ -18,20 +21,77 @@ export default function CSRPage() {
       >
         <XMarkIcon className={"w-5 h-5"} />
       </button>
-      <div
-        className={
-          "px-4 md:px-10 absolute h-full w-full overflow-y-auto pb-40 space-y-20 flex items-center justify-center bg-black z-10"
-        }
-      >
-        <Eye1 />
-        {/*<Eye2 />*/}
-        {/*<Eye3 />*/}
-        {/*<Eye4 />*/}
-        {/*<Eye5 />*/}
-        {/*<Eye6 />*/}
-        {/*<Eye7 />*/}
-        {/*<Eye8 />*/}
-      </div>
+      <Canvas>
+        <ambientLight />
+        <color attach="background" args={["black"]} />
+        <pointLight position={[10, 10, 10]} />
+        <Torus />
+        <OrbitControls />
+        <AsciiRenderer fgColor="white" bgColor="black" />
+      </Canvas>
     </>
   );
+}
+
+function Torus() {
+  const ref = useRef();
+  useFrame((state, delta) => {
+    // @ts-ignore
+    ref.current.rotation.x = ref.current.rotation.y += delta / 2;
+  });
+
+  return (
+    // @ts-ignore
+    <mesh ref={ref} scale={1}>
+      <torusGeometry args={[1, 0.4, 12, 48]} />
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+}
+
+function AsciiRenderer({
+  renderIndex = 1,
+  characters = " A",
+  invert = true,
+  color = false,
+  resolution = 0.15,
+  fgColor,
+  bgColor,
+}: any) {
+  const { size, gl, scene, camera } = useThree();
+  const effect = useMemo(() => {
+    const effect = new AsciiEffect(gl, characters, {
+      invert,
+      color,
+      resolution,
+    });
+    effect.domElement.style.position = "absolute";
+    effect.domElement.style.top = "0px";
+    effect.domElement.style.left = "0px";
+    effect.domElement.style.pointerEvents = "none";
+    return effect;
+  }, [characters, invert, color, resolution]);
+  useLayoutEffect(() => {
+    effect.domElement.style.color = fgColor;
+    effect.domElement.style.backgroundColor = bgColor;
+  }, [fgColor, bgColor]);
+  useEffect(() => {
+    gl.domElement.style.opacity = "0";
+    // @ts-ignore
+    gl.domElement.parentNode.appendChild(effect.domElement);
+    return () => {
+      gl.domElement.style.opacity = "1";
+      // @ts-ignore
+      gl.domElement.parentNode.removeChild(effect.domElement);
+    };
+  }, [effect]);
+  useEffect(() => {
+    effect.setSize(size.width, size.height);
+  }, [effect, size]);
+
+  useFrame((state) => {
+    effect.render(scene, camera);
+  }, renderIndex);
+
+  return null;
 }
