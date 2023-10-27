e6ad7cbc7181f4d83ae477f35533e5d30d908852
tunogya <20251942+tunogya@users.noreply.github.com>
2023-10-27 11:12:09 +0800

    add Credit

--- app/api/dashboard/balance/route.ts
+++ app/api/dashboard/balance/route.ts
@@ -1,46 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSession } from "@auth0/nextjs-auth0";
-import ddbDocClient from "@/utils/ddbDocClient";
-import { GetCommand } from "@aws-sdk/lib-dynamodb";
+import { getSession } from "@auth0/nextjs-auth0/edge";
 import redisClient from "@/utils/redisClient";
+
+export const runtime = "edge";
 
 const GET = async (req: NextRequest) => {
   const session = await getSession();
   const sub = session?.user.sub;
 
-  const cache = await redisClient.get(`${sub}:balance`);
-  const bonus = await redisClient.get(`${sub}:bonus`);
+  const [cache, bonus, credit] = await redisClient.mget(
+    `${sub}:balance`,
+    `${sub}:bonus`,
+    `${sub}:credit`,
+  );
 
-  if (cache) {
-    return NextResponse.json({
-      balance: (Number(cache) || 0) + (Number(bonus) || 0),
-    });
-  }
-
-  try {
-    const { Item } = await ddbDocClient.send(
-      new GetCommand({
-        TableName: "abandonai-prod",
-        Key: {
-          PK: `USER#${sub}`,
-          SK: `BALANCE`,
-        },
-      }),
-    );
-
-    return NextResponse.json({
-      balance: (Item?.balance || 0) + (Item?.bonus || 0),
-    });
-  } catch (e) {
-    return NextResponse.json(
-      {
-        error: "Internal Server Error",
-      },
-      {
-        status: 500,
-      },
-    );
-  }
+  return NextResponse.json({
+    balance: (Number(cache) || 0) + (Number(bonus) || 0),
+    credit: credit || 0,
+  });
 };
 
 export { GET };
