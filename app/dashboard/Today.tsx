346dc73033d9f6857aaaef7e8bd7cb388d6d620b
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-05 19:09:45 +0800

    update

--- app/dashboard/Today.tsx
+++ app/dashboard/Today.tsx
@@ -1,20 +1,33 @@
+"use client";
 import React from "react";
 import dynamic from "next/dynamic";
+import useSWR from "swr";
 import {
-  Chart,
-  CreditBalance,
-  TodayCost,
-  USDBalance,
-  YesterdayCost,
-} from "@/app/dashboard/TodayComponents";
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ReferenceLine,
+  ResponsiveContainer,
+  XAxis,
+} from "recharts";
+import { roundUp } from "@/utils/roundUp";
 
 const DepositButton = dynamic(() => import("@/components/DepositButton"), {
   ssr: false,
 });
 
-export const runtime = "edge";
+const CSR = () => {
+  const { data: costData } = useSWR("/api/dashboard/cost", (url) =>
+    fetch(url).then((res) => res.json()),
+  );
+  const { data: balanceData } = useSWR("/api/dashboard/balance", (url) =>
+    fetch(url).then((res) => res.json()),
+  );
+  const { data: todayData } = useSWR("/api/dashboard/today", (url) =>
+    fetch(url).then((res) => res.json()),
+  );
+  const timestamp = new Date().getUTCHours();
 
-const SSR = () => {
   return (
     <div className={"flex flex-col gap-2 xl:gap-8"}>
       <div
@@ -31,16 +44,56 @@ const SSR = () => {
               <div className={"flex justify-between items-center text-sm"}>
                 <div className={"text-gray-600"}>Gross costs (UTC+0)</div>
               </div>
-              <TodayCost />
+              <div className={"text-gray-400 select-text"}>
+                ${costData?.today || "0"}
+              </div>
             </div>
             <div className={"flex flex-col gap-1"}>
               <div className={"flex justify-between items-center text-sm"}>
                 <div className={"text-gray-600"}>Yesterday</div>
               </div>
-              <YesterdayCost />
+              <div className={"text-gray-400 select-text"}>
+                ${costData?.yesterday || "0"}
+              </div>
             </div>
           </div>
-          <Chart />
+          <div className={"h-[128px] w-full mt-4"}>
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart data={todayData || []}>
+                <CartesianGrid strokeOpacity={0.5} horizontal={false} />
+                <XAxis
+                  dataKey="hour"
+                  axisLine={{
+                    stroke: "#c9c9c9",
+                  }}
+                  type={"number"}
+                  tickLine={{
+                    stroke: "#c9c9c9",
+                  }}
+                  tick={{ fontSize: "10px" }}
+                  height={16}
+                />
+                <Bar
+                  stackId="a"
+                  strokeWidth={2}
+                  dataKey={"gpt4"}
+                  fill={"#AB68FF"}
+                />
+                <ReferenceLine
+                  x={timestamp}
+                  stroke="#0066FF"
+                  strokeDasharray={"4 4"}
+                  opacity={0.5}
+                />
+                <Bar
+                  stackId="a"
+                  strokeWidth={2}
+                  dataKey={"gpt3_5"}
+                  fill={"#19C37D"}
+                />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
         </div>
         <div className={"flex flex-col flex-1 min-w-fit"}>
           <div
@@ -52,7 +105,10 @@ const SSR = () => {
               <div className={"text-gray-600"}>USD Balance</div>
               <DepositButton className={"text-[#0066FF]"} />
             </div>
-            <USDBalance />
+            <div className={"text-xl text-gray-800 select-text"}>
+              {balanceData?.balance < 0 ? "-" : ""}$
+              {roundUp(Math.abs(balanceData?.balance) || 0, 6)}
+            </div>
             <div className={"text-xs text-gray-400"}>Advanced pay balance</div>
           </div>
           <div className={"h-[105px] pl-5 pt-5 w-full flex flex-col gap-1"}>
@@ -60,7 +116,9 @@ const SSR = () => {
               <div className={"text-gray-600"}>Credit Points</div>
               {/*<div className={"text-[#0066FF]"}>View</div>*/}
             </div>
-            <CreditBalance />
+            <div className={"text-xl text-gray-800 select-text"}>
+              {roundUp(Math.abs(balanceData?.credit) || 0, 6)} ABD
+            </div>
             <div className={"text-xs text-gray-400"}>Abandon credit points</div>
           </div>
         </div>
@@ -69,4 +127,4 @@ const SSR = () => {
   );
 };
 
-export default SSR;
+export default CSR;
