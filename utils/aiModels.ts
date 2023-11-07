fe0aadb1e1286fe11cc7319fc444d3acf8146022
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-07 10:21:22 +0800

    update

--- utils/aiModels.ts
+++ utils/aiModels.ts
@@ -8,59 +8,11 @@ export const AI_MODELS_MAP = new Map([
     },
   ],
   [
-    "gpt-4-0314",
-    {
-      content: 8192,
-      input_price: 0.03,
-      output_price: 0.06,
-    },
-  ],
-  [
-    "gpt-4-0613",
-    {
-      content: 8192,
-      input_price: 0.03,
-      output_price: 0.06,
-    },
-  ],
-  [
-    "gpt-3.5-turbo-0613",
-    {
-      content: 4092,
-      input_price: 0.0015,
-      output_price: 0.0002,
-    },
-  ],
-  [
-    "gpt-3.5-turbo-0301",
-    {
-      content: 4092,
-      input_price: 0.0015,
-      output_price: 0.0002,
-    },
-  ],
-  [
     "gpt-3.5-turbo",
     {
-      content: 4092,
-      input_price: 0.0015,
-      output_price: 0.0002,
-    },
-  ],
-  [
-    "gpt-3.5-turbo-16k",
-    {
       content: 16368,
-      input_price: 0.003,
-      output_price: 0.004,
-    },
-  ],
-  [
-    "gpt-3.5-turbo-16k-0613",
-    {
-      content: 16368,
-      input_price: 0.003,
-      output_price: 0.004,
+      input_price: 0.001,
+      output_price: 0.002,
     },
   ],
 ]);
