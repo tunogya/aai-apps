8f714522cb17ddabfe6fc263a3d40aa8947a9087
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-07 23:36:05 +0800

    update

--- app/personas/create/page.tsx
+++ app/personas/create/page.tsx
@@ -1,8 +1,17 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import { Listbox } from "@headlessui/react";
+import { ChevronUpDownIcon } from "@heroicons/react/24/solid";
+
+const models = [
+  { id: 1, name: "gpt-3.5-turbo" },
+  { id: 2, name: "gpt-4-1106-preview" },
+];
 
 export default function CSRPage() {
+  const [selectModel, setSelectModel] = useState(models[0]);
+
   return (
     <div className={"h-full w-full"}>
       <div
@@ -37,15 +46,104 @@ export default function CSRPage() {
             Create new Persona
           </div>
         </div>
-        <div className={"flex items-center"}>
+        <div className={"flex items-center gap-3"}>
           <button
             className={
-              "bg-[#0066FF] px-2 py-1 rounded-lg text-white disabled:opacity-50"
+              "px-2 py-1 rounded-lg text-gray-800 border cursor-pointer font-medium"
+            }
+            disabled
+          >
+            Cancel
+          </button>
+          <button
+            className={
+              "bg-[#0066FF] px-2 py-1 rounded-lg text-white disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer font-medium"
             }
             disabled
           >
             Create
           </button>
+        </div>
+      </div>
+      <div className={"flex h-full"}>
+        <div className={"w-1/2 min-w-[440px] flex justify-center"}>
+          <div className={"p-10 min-w-[360px] w-[608px] flex flex-col gap-8"}>
+            <div className={"flex flex-col gap-4 text-gray-800"}>
+              <div className={"text-xl font-medium"}>Name</div>
+              <input
+                className={
+                  "border text-sm overflow-x-scroll max-w-[360px] w-full h-7 px-2 py-1 rounded focus:outline-[#0066FF]"
+                }
+              />
+            </div>
+            <div className={"flex flex-col gap-4 text-gray-800"}>
+              <div className={"text-xl font-medium"}>Instructions</div>
+              <textarea
+                className={
+                  "border text-sm overflow-x-scroll max-w-[360px] w-full h-7 px-2 py-1 rounded focus:outline-[#0066FF] min-h-[80px] max-h-[240px]"
+                }
+              />
+            </div>
+            <div className={"flex flex-col gap-4 text-gray-800"}>
+              <div className={"text-xl font-medium"}>Model</div>
+              <Listbox value={selectModel} onChange={setSelectModel}>
+                <div className="relative mt-1">
+                  <Listbox.Button
+                    className={
+                      "relative text-sm border max-w-[360px] w-full h-7 px-2 py-1 text-start rounded hover:outline hover:outline-[#0066FF]"
+                    }
+                  >
+                    <div className={"flex items-center justify-between"}>
+                      {selectModel.name}
+                      <ChevronUpDownIcon className={"w-4 h-4"} />
+                    </div>
+                  </Listbox.Button>
+                  <Listbox.Options
+                    className={
+                      "absolute mt-1 max-h-60 max-w-[360px] w-full overflow-auto rounded bg-white shadow border py-1"
+                    }
+                  >
+                    {models.map((m) => (
+                      <Listbox.Option
+                        key={m.id}
+                        value={m}
+                        className={
+                          "px-2 py-1 hover:bg-gray-100 cursor-pointer text-sm"
+                        }
+                      >
+                        {m.name}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </div>
+              </Listbox>
+            </div>
+            <div className={"flex flex-col gap-4 text-gray-800"}>
+              <div className={"text-xs font-medium text-gray-500"}>Tools</div>
+              <div>
+                <div className={"text-sm font-medium border-t py-2"}>
+                  Function
+                </div>
+                <div className={"text-sm font-medium border-t py-2"}>
+                  Code interpreter
+                </div>
+                <div className={"text-sm font-medium border-t py-2"}>
+                  Retrieval
+                </div>
+                <div
+                  className={"text-xs font-medium border-y py-4 text-gray-400"}
+                >
+                  FILES
+                </div>
+                <div className={"text-gray-400 text-sm py-2"}>
+                  Add files to use with code interpreter or retrieval.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={"w-1/2 bg-gray-100 p-10"}>
+          <div className={"text-xl text-gray-800 font-medium"}>Test</div>
         </div>
       </div>
     </div>
