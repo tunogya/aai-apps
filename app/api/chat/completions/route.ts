046782163177f450251caceaae9b88103a46a398
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-08 17:36:55 +0800

    update

--- app/api/chat/completions/route.ts
+++ app/api/chat/completions/route.ts
@@ -65,178 +65,123 @@ export async function POST(req: Request): Promise<Response> {
   const openai = new OpenAIApi(configuration);
 
   try {
-    if (config?.stream) {
-      const res = await openai.createChatCompletion(config);
-      const stream = OpenAIStream(res, {
-        onCompletion(completion) {
-          // record usage log and reduce the balance of user
-          sqsClient.send(
-            new SendMessageBatchCommand({
-              QueueUrl: process.env.AI_DB_UPDATE_SQS_FIFO_URL,
-              Entries: [
-                {
-                  Id: `update-usage-${new Date().getTime()}`,
-                  MessageBody: JSON.stringify({
-                    TableName: "abandonai-prod",
-                    Item: {
-                      PK: `USER#${sub}`,
-                      SK: `USAGE#${new Date().toISOString()}`,
-                      prompt: config.messages,
-                      completion,
-                      model: config.model,
-                      created: Math.floor(Date.now() / 1000),
-                      TTL: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 31, // 31 days
-                    },
-                    ConditionExpression: "attribute_not_exists(#PK)",
-                    ExpressionAttributeNames: {
-                      "#PK": "PK",
-                    },
-                  }),
-                  MessageAttributes: {
-                    Command: {
-                      DataType: "String",
-                      StringValue: "PutCommand",
-                    },
-                  },
-                  MessageGroupId: "update-usage",
-                },
-              ],
+    const res = await openai
+      .createChatCompletion(config)
+      .then((res) => res.json());
+
+    const id = res.id;
+    const { prompt_tokens, completion_tokens, total_tokens } = res.usage;
+
+    const prompt_cost = roundUp(
+      (prompt_tokens / 1000) * AI_MODELS_MAP.get(config.model)!.input_price,
+      6,
+    );
+    const completion_cost = roundUp(
+      (completion_tokens / 1000) *
+        AI_MODELS_MAP.get(config.model)!.output_price,
+      6,
+    );
+    const fee_cost = roundUp((prompt_cost + completion_cost) * FEE_RATE, 6);
+    const total_cost = roundUp(prompt_cost + completion_cost + fee_cost, 6);
+
+    sqsClient.send(
+      new SendMessageBatchCommand({
+        QueueUrl: process.env.AI_DB_UPDATE_SQS_FIFO_URL,
+        Entries: [
+          {
+            Id: `update-usage-${new Date().getTime()}`,
+            MessageBody: JSON.stringify({
+              TableName: "abandonai-prod",
+              Item: {
+                PK: `USER#${sub}`,
+                SK: `USAGE#${new Date().toISOString()}`,
+                model: config.model,
+                prompt_tokens,
+                completion_tokens,
+                total_tokens,
+                fee_cost,
+                prompt_cost,
+                completion_cost,
+                total_cost,
+                created: Math.floor(Date.now() / 1000),
+                TTL: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 31, // 31 days
+              },
+              ConditionExpression: "attribute_not_exists(#PK)",
+              ExpressionAttributeNames: {
+                "#PK": "PK",
+              },
             }),
-          );
-        },
-      });
-
-      return new StreamingTextResponse(stream);
-    } else {
-      const res = await openai
-        .createChatCompletion(config)
-        .then((res) => res.json());
-
-      const id = res.id;
-      const { prompt_tokens, completion_tokens, total_tokens } = res.usage;
-
-      const prompt_cost = roundUp(
-        (prompt_tokens / 1000) * AI_MODELS_MAP.get(config.model)!.input_price,
-        6,
-      );
-      const completion_cost = roundUp(
-        (completion_tokens / 1000) *
-          AI_MODELS_MAP.get(config.model)!.output_price,
-        6,
-      );
-      const fee_cost = roundUp((prompt_cost + completion_cost) * FEE_RATE, 6);
-      const total_cost = roundUp(prompt_cost + completion_cost + fee_cost, 6);
-
-      await sqsClient.send(
-        new SendMessageBatchCommand({
-          QueueUrl: process.env.AI_DB_UPDATE_SQS_FIFO_URL,
-          Entries: [
-            {
-              Id: `update-usage-${new Date().getTime()}`,
-              MessageBody: JSON.stringify({
-                TableName: "abandonai-prod",
-                Item: {
-                  PK: `USER#${sub}`,
-                  SK: `USAGE#${new Date().toISOString()}`,
-                  model: config.model,
-                  prompt_tokens,
-                  completion_tokens,
-                  total_tokens,
-                  fee_cost,
-                  prompt_cost,
-                  completion_cost,
-                  total_cost,
-                  created: Math.floor(Date.now() / 1000),
-                  TTL: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 31, // 31 days
-                },
-                ConditionExpression: "attribute_not_exists(#PK)",
-                ExpressionAttributeNames: {
-                  "#PK": "PK",
-                },
-              }),
-              MessageAttributes: {
-                Command: {
-                  DataType: "String",
-                  StringValue: "PutCommand",
-                },
+            MessageAttributes: {
+              Command: {
+                DataType: "String",
+                StringValue: "PutCommand",
               },
-              MessageGroupId: "update-usage",
             },
-            {
-              Id: `${id}-update-balance`,
-              MessageBody: JSON.stringify({
-                TableName: "abandonai-prod",
-                Key: {
-                  PK: `USER#${sub}`,
-                  SK: "BALANCE",
-                },
-                UpdateExpression: "ADD #balance :total_cost",
-                ExpressionAttributeNames: {
-                  "#balance": "balance",
-                },
-                ExpressionAttributeValues: {
-                  ":total_cost": -1 * total_cost,
-                },
-              }),
-              MessageAttributes: {
-                Command: {
-                  DataType: "String",
-                  StringValue: "UpdateCommand",
-                },
+            MessageGroupId: "update-usage",
+          },
+          {
+            Id: `${id}-update-balance`,
+            MessageBody: JSON.stringify({
+              TableName: "abandonai-prod",
+              Key: {
+                PK: `USER#${sub}`,
+                SK: "BALANCE",
               },
-              MessageDeduplicationId: `${id}-update-balance`,
-              MessageGroupId: "update-balance",
-            },
-            {
-              Id: `${id}-update-credit`,
-              MessageBody: JSON.stringify({
-                TableName: "abandonai-prod",
-                Key: {
-                  PK: `USER#${sub}`,
-                  SK: "CREDIT",
-                },
-                UpdateExpression: "ADD #balance :total_cost",
-                ExpressionAttributeNames: {
-                  "#balance": "balance",
-                },
-                ExpressionAttributeValues: {
-                  ":total_cost": total_cost * 20,
-                },
-              }),
-              MessageAttributes: {
-                Command: {
-                  DataType: "String",
-                  StringValue: "UpdateCommand",
-                },
+              UpdateExpression: "ADD #balance :total_cost",
+              ExpressionAttributeNames: {
+                "#balance": "balance",
               },
-              MessageDeduplicationId: `${id}-update-credit`,
-              MessageGroupId: "update-credit",
+              ExpressionAttributeValues: {
+                ":total_cost": -1 * total_cost,
+              },
+            }),
+            MessageAttributes: {
+              Command: {
+                DataType: "String",
+                StringValue: "UpdateCommand",
+              },
             },
-          ],
-        }),
-      );
+            MessageDeduplicationId: `${id}-update-balance`,
+            MessageGroupId: "update-balance",
+          },
+          {
+            Id: `${id}-update-credit`,
+            MessageBody: JSON.stringify({
+              TableName: "abandonai-prod",
+              Key: {
+                PK: `USER#${sub}`,
+                SK: "CREDIT",
+              },
+              UpdateExpression: "ADD #balance :total_cost",
+              ExpressionAttributeNames: {
+                "#balance": "balance",
+              },
+              ExpressionAttributeValues: {
+                ":total_cost": total_cost * 20,
+              },
+            }),
+            MessageAttributes: {
+              Command: {
+                DataType: "String",
+                StringValue: "UpdateCommand",
+              },
+            },
+            MessageDeduplicationId: `${id}-update-credit`,
+            MessageGroupId: "update-credit",
+          },
+        ],
+      }),
+    );
 
-      const newTotalCost = await redisClient.incrbyfloat(
-        `${sub}:total_cost:${new Date().toISOString().slice(0, 10)}`,
-        total_cost,
-      );
-      if (newTotalCost === total_cost) {
-        await redisClient.expire(
-          `${sub}:total_cost:${new Date().toISOString().slice(0, 10)}`,
-          86400 * 7,
-        );
-      }
-
-      await redisClient.set(hash, JSON.stringify(res), {
-        ex: 60 * 60 * 24 * 7,
-      });
-      return new Response(JSON.stringify(res), {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    }
+    redisClient.set(hash, JSON.stringify(res), {
+      ex: 60 * 60 * 24 * 7,
+    });
+    return new Response(JSON.stringify(res), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
   } catch (e) {
     console.log(e);
     return new Response("Internal Server Error", {
