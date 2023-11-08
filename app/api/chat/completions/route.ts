c32e3c37acb8f210f3c5a1be46096a3bc6fe550e
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-08 19:50:24 +0800

    update

--- app/api/chat/completions/route.ts
+++ app/api/chat/completions/route.ts
@@ -1,32 +1,12 @@
 import { Configuration, OpenAIApi } from "openai-edge";
-import { SQSClient } from "@aws-sdk/client-sqs";
 import redisClient from "@/utils/redisClient";
-import { AI_MODELS_MAP } from "@/utils/aiModels";
-
-const sqsClient = new SQSClient({
-  region: "ap-northeast-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
-});
 
 export const runtime = "edge";
-
-const FEE_RATE = 0.3;
 
 // @ts-ignore
 export async function POST(req: Request): Promise<Response> {
   const bearerToken = req.headers.get("Authorization");
   if (!bearerToken) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
-  }
-  const token = bearerToken.split(" ")[1];
-
-  const sub = (await redisClient.get(`${token}:sub`)) as string;
-  if (!sub) {
     return new Response("Unauthorized", {
       status: 401,
     });
@@ -57,21 +37,6 @@ export async function POST(req: Request): Promise<Response> {
       .createChatCompletion(config)
       .then((res) => res.json());
 
-    const id = res.id;
-    const { prompt_tokens, completion_tokens, total_tokens } = res.usage;
-
-    const prompt_cost = roundUp(
-      (prompt_tokens / 1000) * AI_MODELS_MAP.get(config.model)!.input_price,
-      6,
-    );
-    const completion_cost = roundUp(
-      (completion_tokens / 1000) *
-        AI_MODELS_MAP.get(config.model)!.output_price,
-      6,
-    );
-    const fee_cost = roundUp((prompt_cost + completion_cost) * FEE_RATE, 6);
-    const total_cost = roundUp(prompt_cost + completion_cost + fee_cost, 6);
-
     return new Response(JSON.stringify(res), {
       status: 200,
       headers: {
@@ -84,11 +49,6 @@ export async function POST(req: Request): Promise<Response> {
       status: 500,
     });
   }
-}
-
-function roundUp(num: number, decimals: number): number {
-  const pow = Math.pow(10, decimals);
-  return Math.ceil(num * pow) / pow;
 }
 
 async function calculateHash(text: string) {
