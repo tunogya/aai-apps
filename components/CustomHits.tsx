79a7516f41f48044122e461ae2673f17278f16b4
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-11 23:46:29 +0800

    update

--- components/CustomHits.tsx
+++ components/CustomHits.tsx
@@ -1,4 +1,4 @@
-import { Highlight, Snippet, useHits } from "react-instantsearch";
+import { useHits } from "react-instantsearch";
 import Link from "next/link";
 import React from "react";
 
@@ -9,30 +9,13 @@ function Hit({ hit }: any) {
     <Link href={"/chat/" + id}>
       <div className={"text-sm px-4 py-3 text-gray-800 hover:bg-gray-100"}>
         <div className={"flex items-start gap-1"}>
-          <div className={"w-6 shrink-0 pt-1"}>
-            <svg
-              stroke="currentColor"
-              fill="none"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="h-4 w-4"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-            </svg>
-          </div>
           <div className={"text-sm"}>
-            <div className={"truncate"}>{hit.messages[0].content}</div>
             {hit._snippetResult.messages
               .filter((item: any) => item.content.matchLevel === "full")
               .map((item: any, index: number) => (
                 <div
                   key={index}
-                  className={"truncate text-gray-500"}
+                  className={"line-clamp-2 text-gray-500"}
                   dangerouslySetInnerHTML={{
                     __html: item.content.value,
                   }}
