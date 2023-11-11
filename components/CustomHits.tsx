200807e8fe3a99deb397a4338fcab4ffe35abedc
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-12 00:47:04 +0800

    update

--- components/CustomHits.tsx
+++ components/CustomHits.tsx
@@ -4,22 +4,36 @@ import React from "react";
 
 function Hit({ hit }: any) {
   const id = hit.objectID;
+
   return (
     <Link href={"/chat/" + id}>
-      <div className={"text-sm px-4 py-3 text-gray-800 hover:bg-gray-100"}>
+      <div className={"text-sm px-4 py-3 text-gray-800 border-b"}>
         <div className={"flex items-start gap-1"}>
           <div className={"text-sm"}>
-            {hit?._snippetResult?.messages
-              ?.filter((item: any) => item.content.matchLevel === "full")
-              ?.map((item: any, index: number) => (
-                <div
-                  key={index}
-                  className={"line-clamp-2 text-gray-500"}
-                  dangerouslySetInnerHTML={{
-                    __html: item.content.value,
-                  }}
-                />
-              ))}
+            <div className={"flex justify-between items-center"}>
+              <div className={"truncate text-[#0066FF] font-medium text-lg"}>
+                {hit?.title}
+              </div>
+              <div className={"text-gray-600 text-xs"}>
+                {new Date(Number(hit?.updated) * 1000).toLocaleString()}
+              </div>
+            </div>
+            <div className={"flex flex-col space-y-1 mt-2"}>
+              {hit?._snippetResult?.messages
+                ?.slice(1)
+                ?.filter((item: any) => item?.content?.matchLevel === "full")
+                ?.map((item: any, index: number) => (
+                  <span
+                    key={index}
+                    className={`p-2 bg-gray-100 rounded ${
+                      item?.role?.value === "user" ? "ml-4" : "mr-4"
+                    }`}
+                    dangerouslySetInnerHTML={{
+                      __html: item?.content?.value,
+                    }}
+                  />
+                ))}
+            </div>
           </div>
         </div>
       </div>
@@ -35,7 +49,7 @@ const CustomHits = (props: any) => {
   }
 
   return (
-    <div className="absolute mt-2 bg-white border shadow w-[600px] rounded-lg z-20 h-[50vh] overflow-y-auto">
+    <div className="absolute mt-2 bg-white border shadow w-[600px] rounded-lg z-20 h-[50vh] overflow-y-auto overflow-x-hidden">
       <div className={"px-4 py-2 border-b"}>
         <div className={"font-semibold text-gray-800"}>Chats</div>
       </div>
