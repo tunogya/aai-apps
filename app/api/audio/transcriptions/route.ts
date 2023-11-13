34f8939d64eae396f66fd132e703ce25a1f7dadb
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-14 00:58:37 +0800

    update

--- app/api/audio/transcriptions/route.ts
+++ app/api/audio/transcriptions/route.ts
@@ -1,15 +1,9 @@
-import { getSession } from "@auth0/nextjs-auth0/edge";
 import OpenAI from "openai";
-import fs from "fs";
+
+export const runtime = "edge";
 
 // @ts-ignore
 export async function POST(req: Request): Promise<Response> {
-  // @ts-ignore
-  const { user } = await getSession();
-  const sub = user.sub;
-
-  // let {model, file} = await req.json();
-
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
