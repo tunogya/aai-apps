31e37437c828f08b2ca32fa60fc4bb0a7943ec6d
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-14 09:52:32 +0800

    update

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="w-full h-full space-y-20 justify-center items-center py-4 px-3">
+    <div className="w-full h-full space-y-20 justify-center items-center py-4 px-3 flex flex-col">
       <div className="text-center text-2xl md:text-3xl font-serif">Oops!</div>
       <Link
         href={"/auth/login"}
