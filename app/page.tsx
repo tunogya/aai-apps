31bdd7f70b8a4d78844b37bfe0748bcaee0126d0
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-16 15:10:40 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { redirect } from "next/navigation";
+import { nanoid } from "ai";
 
 export default function Index() {
   const { user, error, isLoading } = useUser();
@@ -9,7 +10,7 @@ export default function Index() {
 
   if (error) return redirect(`/auth/error?message=${error.message}`);
 
-  if (user) return redirect("/chat");
+  if (user) return redirect(`/chat/${nanoid()}`);
 
   return redirect("/auth/login");
 }
