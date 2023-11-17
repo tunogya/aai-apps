d42a8632fc9d14649c4f3f657f998c26f83f252c
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-17 21:12:11 +0800

    update

--- app/pay/error/page.tsx
+++ app/pay/error/page.tsx
@@ -1,8 +1,8 @@
-"use client";
 import Link from "next/link";
-import { TopUpButton } from "@/app/components/TopUpButton";
 import React from "react";
 import { useSearchParams } from "next/navigation";
+
+export const runtime = "edge";
 
 export default function CSRPage() {
   const searchParams = useSearchParams();
