084b3cb0c4d1d9fcbfd54d0e3fd0964d66253196
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-26 14:04:07 +0800

    update

--- app/assistants/(wrap)/[id]/Events.tsx
+++ app/assistants/(wrap)/[id]/Events.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const CSRPage = () => {
-  return <div></div>;
+  return (
+    <div className={"space-y-6 pt-4"}>
+      <div className={"text-sm text-gray-500"}>No Events</div>
+    </div>
+  );
 };
 
 export default CSRPage;
--- app/assistants/(wrap)/[id]/Overview.tsx
+++ app/assistants/(wrap)/[id]/Overview.tsx
@@ -2,13 +2,19 @@
 
 const CSRPage = () => {
   return (
-    <div className={"space-y-6"}>
+    <div className={"space-y-6 pt-4"}>
       <div>
-        <div className={"flex justify-between items-center pt-4 pb-3 border-b"}>
+        <div className={"flex justify-between items-center pb-3 border-b"}>
           <div className={"font-semibold text-gray-800"}>Telegram Bots</div>
-          <button className={"text-sm text-[#0066FF]"}>Add</button>
+          <button
+            disabled
+            className={
+              "text-sm text-[#0066FF] disabled:cursor-auto disabled:opacity-50"
+            }
+          >
+            Add
+          </button>
         </div>
-
         <div className={"pt-3 text-sm text-gray-500"}>No Bots</div>
       </div>
     </div>
