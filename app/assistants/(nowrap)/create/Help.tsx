ebb5bad18f201aa3a9497021f233d924e7ed3d40
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-26 13:58:51 +0800

    update

--- app/assistants/(nowrap)/create/Help.tsx
+++ app/assistants/(nowrap)/create/Help.tsx
@@ -4,35 +4,17 @@ export const runtime = "edge";
 
 const Help = () => {
   return (
-    <article className="prose">
+    <article className="prose prose-gray">
       <h1>
         What is Assistants: Unleashing the Power of AI in Your Applications
       </h1>
       <p>
         In an era where artificial intelligence is not just a buzzword but a
-        digital co-pilot in our daily lives, the introduction of the Assistants
+        digital copilot in our daily lives, the introduction of the Assistants
         API marks a revolutionary leap forward. Developed with the vision to
         help creators infuse agent-like experiences into their own applications,
         the Assistants API by OpenAI presents an innovative approach to building
         AI-driven interactions.
-      </p>
-      <h2>The Essence of an Assistant</h2>
-      <p>
-        An Assistant is more than a mere chatbot; it&apos;s a purpose-built AI
-        tailored to execute specific tasks with precision. It&apos;s designed to
-        understand instructions, possess additional knowledge, and utilize
-        models and tools to accomplish a variety of operations. Imagine having a
-        digital entity that can analyze data, assist in coding, plan your
-        vacations, or even control music through voice commands. This is the
-        transformative potential of an Assistant.
-      </p>
-      <h2>Core Features and Capabilities</h2>
-      <p>
-        The Assistants API is a powerhouse that simplifies the complex. With
-        features like Code Interpreter and Retrieval, and the ability to call
-        functions directly, it shoulders the burden of heavy lifting that
-        developers used to manage themselves. This enables the creation of
-        high-quality AI apps that were once a stretch for the imagination.
       </p>
       <p>
         Our platform, AbandonAI, harnesses this incredible technology to provide
@@ -41,25 +23,6 @@ const Help = () => {
         entities, driving your social media bots and enhancing user interactions
         with AI&apos;s intelligence.
       </p>
-      <h2>Flexibility and Range of Use Cases</h2>
-      <p>
-        Whether it&apos;s a natural language-based data analysis app, a coding
-        assistant, an AI-powered vacation planner, or a voice-controlled DJ, the
-        Assistants API is designed for flexibility. Its capabilities are built
-        on the same foundation that powers GPTs products, which means custom
-        instructions and tools like Code interpreter, Retrieval, and function
-        calling are at your disposal.
-      </p>
-      <h2>Persistent Threads for Seamless Integration</h2>
-      <p>
-        A significant innovation introduced by the Assistants API is the concept
-        of persistent and infinitely long threads. This feature allows
-        developers to offload thread state management to OpenAI, circumventing
-        the limitations of context windows. With the Assistants API, the
-        continuity of conversations is maintained effortlessly, as developers
-        can simply add each new message to an existing thread.
-      </p>
-      <h2>The Value Proposition</h2>
       <p>
         By leveraging our platform, users can unlock the full potential of the
         Assistants API. The value lies in the seamless integration into everyday
@@ -72,13 +35,6 @@ const Help = () => {
         identity, capable of amplifying your presence across various social
         platforms. The future of AI interaction is here, and it&apos;s
         personalized, persistent, and powerfully simple.
-      </p>
-      <p>
-        In summary, Assistants are not just tools; they are your partners in the
-        digital realm. They bring a level of sophistication to your applications
-        that will delight, engage, and serve your users in ways previously
-        unimagined. Welcome to the future of application interaction, where
-        Assistants are at the heart of the experience.
       </p>
     </article>
   );
