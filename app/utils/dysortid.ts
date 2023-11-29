ada32931af152ff078e0ac35eb8ffce312237e2e
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-29 17:00:10 +0800

    update dysortid

--- app/utils/dysortid.ts
+++ app/utils/dysortid.ts
@@ -1,9 +1,23 @@
 import { nanoid } from "ai";
 
+const encodeToBase62 = (num: number, size = 8) => {
+  const chars =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let encoded = "";
+  while (num > 0) {
+    const remainder = num % 62;
+    encoded = chars[remainder] + encoded;
+    num = Math.floor(num / 62);
+  }
+  while (encoded.length < size) {
+    encoded = "0" + encoded;
+  }
+  return encoded;
+};
+
 const dysortid = () => {
   const timestamp = new Date().getTime();
-  const id = nanoid();
-  return `${timestamp}${id}`;
+  return `${encodeToBase62(timestamp, 12)}${nanoid(8)}`;
 };
 
 export default dysortid;
