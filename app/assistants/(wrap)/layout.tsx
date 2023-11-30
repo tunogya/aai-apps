bf8d840b54b495285d5ec9054a363efdb1a0311a
tunogya <20251942+tunogya@users.noreply.github.com>
2023-11-30 14:50:05 +0800

    update

--- app/assistants/(wrap)/layout.tsx
+++ app/assistants/(wrap)/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout(props: { children: ReactNode }) {
     <div className={"h-full w-full flex relative justify-center"}>
       <div className={"max-w-[1920px] w-full h-full flex overflow-hidden"}>
         <PrimaryNav />
-        <div className={"w-full h-full lg:mr-10 relative"}>
+        <div className={"w-full h-full relative"}>
           <Toolbar border={false} />
           {props.children}
         </div>
