4a1131a677a19017e243e1ca38f78d233250c496
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-01 11:10:28 +0800

    update

--- app/chat/[[...id]]/page.tsx
+++ app/chat/[[...id]]/page.tsx
@@ -83,10 +83,10 @@ export default function Chat() {
       >
         <div
           className={
-            "py-2 md:pb-4 w-full px-4 md:px-8 xl:px-20 flex justify-center pl-12 md:pl-0"
+            "py-2 md:pb-4 w-full px-4 md:px-8 xl:px-20 flex justify-center"
           }
         >
-          <div className={"flex w-full max-w-3xl gap-3"}>
+          <div className={"flex w-full max-w-3xl gap-3 pl-8 md:pl-0"}>
             <div className={"w-full flex gap-2"}>
               <div
                 className={`border ${
