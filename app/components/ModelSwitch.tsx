77bbf551218c9d019b612b11b7597db3d42ad158
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-12 12:58:02 +0800

    update

--- app/components/ModelSwitch.tsx
+++ app/components/ModelSwitch.tsx
@@ -48,43 +48,45 @@ const ModelSwitch = () => {
           </button>
         </div>
       )}
-      <div
-        className={`text-sm md:font-semibold md:hover:bg-gray-100 rounded-lg flex items-center gap-2 px-2 py-1.5 select-none justify-between ${
-          !data?.subscription?.isPremium
-            ? "cursor-not-allowed"
-            : "cursor-pointer"
-        }`}
-        onClick={() => {
-          if (useGPT4) {
-            setModel("gpt-3.5-turbo");
-          } else {
-            setModel("gpt-4-1106-preview");
-          }
-        }}
-      >
+      {data?.subscription?.isPremium && (
         <div
-          className={`whitespace-nowrap text-sm font-medium ${
-            useGPT4 ? "text-[#AB68FF]" : "text-gray-800"
+          className={`text-sm md:font-semibold md:hover:bg-gray-100 rounded-lg flex items-center gap-2 px-2 py-1.5 select-none justify-between ${
+            !data?.subscription?.isPremium
+              ? "cursor-not-allowed"
+              : "cursor-pointer"
           }`}
+          onClick={() => {
+            if (useGPT4) {
+              setModel("gpt-3.5-turbo");
+            } else {
+              setModel("gpt-4-1106-preview");
+            }
+          }}
         >
-          GPT-4
-        </div>
-        <Switch
-          checked={useGPT4}
-          disabled={isLoading || !data?.subscription?.isPremium}
-          className={`${
-            useGPT4 ? "bg-[#AB68FF]" : "bg-gray-200"
-          } disabled:cursor-not-allowed
+          <div
+            className={`whitespace-nowrap text-sm font-medium ${
+              useGPT4 ? "text-[#AB68FF]" : "text-gray-800"
+            }`}
+          >
+            GPT-4
+          </div>
+          <Switch
+            checked={useGPT4}
+            disabled={isLoading || !data?.subscription?.isPremium}
+            className={`${
+              useGPT4 ? "bg-[#AB68FF]" : "bg-gray-200"
+            } disabled:cursor-not-allowed
           relative inline-flex h-[14px] w-[24px] shrink-0 cursor-pointer rounded-full border border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
-        >
-          <span className="sr-only">GPT-4</span>
-          <span
-            aria-hidden="true"
-            className={`${useGPT4 ? "translate-x-2.5" : "translate-x-0"}
+          >
+            <span className="sr-only">GPT-4</span>
+            <span
+              aria-hidden="true"
+              className={`${useGPT4 ? "translate-x-2.5" : "translate-x-0"}
             pointer-events-none inline-block h-[12px] w-[12px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
-          />
-        </Switch>
-      </div>
+            />
+          </Switch>
+        </div>
+      )}
     </div>
   );
 };
