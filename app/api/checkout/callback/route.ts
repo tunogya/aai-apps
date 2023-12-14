b00ab0bc99eacd8fb59e761408c3a3c1b48d057e
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-15 01:12:33 +0800

    update

---
+++ app/api/checkout/callback/route.ts
@@ -0,0 +1,221 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+import stripeClient from "@/app/utils/stripeClient";
+import redisClient from "@/app/utils/redisClient";
+import * as process from "process";
+
+const POST = async (req: Request) => {
+  let event: Stripe.Event;
+
+  try {
+    event = stripeClient.webhooks.constructEvent(
+      await (await req.blob()).text(),
+      req.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string,
+    );
+  } catch (err: any) {
+    const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    if (err! instanceof Error) console.log(err);
+    console.log(`❌ Error message: ${errorMessage}`);
+    return NextResponse.json(
+      { message: `Webhook Error: ${errorMessage}` },
+      { status: 400 },
+    );
+  }
+  // Successfully constructed event.
+  console.log("✅ Success:", event.id);
+  try {
+    if (event.type === "checkout.session.completed") {
+      const checkoutSessionCompleted = event.data
+        .object as Stripe.Checkout.Session;
+      const { id, customer_email, livemode } = checkoutSessionCompleted;
+      if (livemode && customer_email) {
+        // get customer info by email
+        const customerInfo = await stripeClient.customers.list({
+          email: customer_email,
+        });
+        const customer = customerInfo?.data?.[0]?.id;
+        if (customer) {
+          const lineItems =
+            await stripeClient.checkout.sessions.listLineItems(id);
+          for (const lineItem of lineItems.data) {
+            const { price } = lineItem;
+            if (price?.id === process.env.ONETIME_PREMIUM_STANDARD_PRICE) {
+              let old_premium_standard_expired = new Date();
+              if (
+                // @ts-ignore
+                customerInfo?.metadata?.premium_standard_expired &&
+                new Date() <
+                  // @ts-ignore
+                  new Date(customerInfo.metadata.premium_standard_expired)
+              ) {
+                old_premium_standard_expired = new Date(
+                  // @ts-ignore
+                  customerInfo.metadata.premium_standard_expired,
+                );
+              }
+              const new_premium_standard_expired = new Date(
+                old_premium_standard_expired.setDate(
+                  old_premium_standard_expired.getDate() + 31,
+                ),
+              ).toISOString();
+              await stripeClient.customers.update(customer as string, {
+                metadata: {
+                  // @ts-ignore
+                  ...(customerInfo?.metadata || {}),
+                  premium_standard_expired: new_premium_standard_expired,
+                },
+              });
+              // @ts-ignore
+              if (customerInfo?.metadata?.id) {
+                await redisClient
+                  .set(
+                    // @ts-ignore
+                    `premium:${customerInfo?.metadata?.id}`,
+                    JSON.stringify({
+                      customer: customerInfo,
+                      subscription: {
+                        isPremium: true,
+                        product: process.env.PREMIUM_STANDARD_PRODUCT,
+                        current_period_start: new Date().getTime() / 1000,
+                        current_period_end:
+                          new Date(new_premium_standard_expired).getTime() /
+                          1000,
+                      },
+                    }),
+                    {
+                      exat:
+                        new Date(new_premium_standard_expired).getTime() / 1000,
+                    },
+                  )
+                  .catch(() => {
+                    console.log("redis error");
+                  });
+              }
+            } else if (price?.id === process.env.ONETIME_PREMIUM_PRO_PRICE) {
+              const customerInfo = await stripeClient.customers.retrieve(
+                customer as string,
+              );
+              let old_premium_pro_expired = new Date();
+              if (
+                // @ts-ignore
+                customerInfo?.metadata?.premium_pro_expired &&
+                new Date() <
+                  // @ts-ignore
+                  new Date(customerInfo.metadata.premium_pro_expired)
+              ) {
+                old_premium_pro_expired = new Date(
+                  // @ts-ignore
+                  customerInfo.metadata.premium_pro_expired,
+                );
+              }
+              const new_premium_pro_expired = new Date(
+                old_premium_pro_expired.setDate(
+                  old_premium_pro_expired.getDate() + 31,
+                ),
+              ).toISOString();
+              await stripeClient.customers.update(customer as string, {
+                metadata: {
+                  // @ts-ignore
+                  ...(customerInfo?.metadata || {}),
+                  premium_pro_expired: new_premium_pro_expired,
+                },
+              });
+              // @ts-ignore
+              if (customerInfo?.metadata?.id) {
+                await redisClient
+                  .set(
+                    // @ts-ignore
+                    `premium:${customerInfo?.metadata?.id}`,
+                    JSON.stringify({
+                      customer: customerInfo,
+                      subscription: {
+                        isPremium: true,
+                        product: process.env.PREMIUM_PRO_PRODUCT,
+                        current_period_start: new Date().getTime() / 1000,
+                        current_period_end:
+                          new Date(new_premium_pro_expired).getTime() / 1000,
+                      },
+                    }),
+                    {
+                      exat: new Date(new_premium_pro_expired).getTime() / 1000,
+                    },
+                  )
+                  .catch(() => {
+                    console.log("redis error");
+                  });
+              }
+            } else if (price?.id === process.env.ONETIME_PREMIUM_MAX_PRICE) {
+              const customerInfo = await stripeClient.customers.retrieve(
+                customer as string,
+              );
+              let old_premium_max_expired = new Date();
+              if (
+                // @ts-ignore
+                customerInfo?.metadata?.premium_max_expired &&
+                new Date() <
+                  // @ts-ignore
+                  new Date(customerInfo.metadata.premium_max_expired)
+              ) {
+                old_premium_max_expired = new Date(
+                  // @ts-ignore
+                  customerInfo.metadata.premium_max_expired,
+                );
+              }
+              const new_premium_max_expired = new Date(
+                old_premium_max_expired.setDate(
+                  old_premium_max_expired.getDate() + 31,
+                ),
+              ).toISOString();
+              await stripeClient.customers.update(customer as string, {
+                metadata: {
+                  // @ts-ignore
+                  ...(customerInfo?.metadata || {}),
+                  premium_max_expired: new_premium_max_expired,
+                },
+              });
+              // @ts-ignore
+              if (customerInfo?.metadata?.id) {
+                await redisClient
+                  .set(
+                    // @ts-ignore
+                    `premium:${customerInfo?.metadata?.id}`,
+                    JSON.stringify({
+                      customer: customerInfo,
+                      subscription: {
+                        isPremium: true,
+                        product: process.env.PREMIUM_MAX_PRODUCT,
+                        current_period_start: new Date().getTime() / 1000,
+                        current_period_end:
+                          new Date(new_premium_max_expired).getTime() / 1000,
+                      },
+                    }),
+                    {
+                      exat: new Date(new_premium_max_expired).getTime() / 1000,
+                    },
+                  )
+                  .catch(() => {
+                    console.log("redis error");
+                  });
+              }
+            }
+          }
+        } else {
+          return NextResponse.json(
+            { message: "404 customer not found" },
+            { status: 200 },
+          );
+        }
+      }
+    }
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      { message: "Webhook handler failed" },
+      { status: 500 },
+    );
+  }
+  return NextResponse.json({ message: "Received" }, { status: 200 });
+};
+
+export { POST };
--- app/premium/page.tsx
+++ app/premium/page.tsx
@@ -3,8 +3,36 @@
 import React from "react";
 import Link from "next/link";
 import dysortid from "@/app/utils/dysortid";
+import useSWR from "swr";
 
 const CSR = () => {
+  const { data, isLoading } = useSWR("/api/customer", (url) =>
+    fetch(url).then((res) => res.json()),
+  );
+
+  if (isLoading || !data?.customer.email) {
+    return (
+      <div
+        className={
+          "w-full h-full flex flex-col items-center justify-center gap-3 animate-pulse text-gray-800"
+        }
+      >
+        <svg
+          width="40"
+          height="40"
+          viewBox="0 0 1024 1024"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M68 68V956H956V68H68ZM142 882V142H586V413.333L512 216H438L216 808H290L345.5 660H586V882H142ZM576.791 586H373.209L475 314.667L576.791 586Z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
+    );
+  }
+
   return (
     <div className={"w-full h-full p-2 md:p-4 relative"}>
       <div className={"p-2 md:p-8 md:border rounded-lg"}>
@@ -12,6 +40,7 @@ const CSR = () => {
         <stripe-pricing-table
           publishable-key="pk_live_51MagF9FPpv8QfieYNE5ZeIQQTIFXZxdRHKyHo8xmWXkYKnYyXKoaxh6BEs5JuNIpgAWU5FNt7d5gyhRrVsxFtqxL00vTDy0spV"
           pricing-table-id="prctbl_1ON4GZFPpv8QfieYARNF5SMG"
+          customer-email={data.customer.email}
         />
       </div>
       <Link href={`/chat/${dysortid()}`}>
