38df1be3fdecc361fc984a107408510b89e11819
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-15 09:47:55 +0800

    update

--- app/premium/page.tsx
+++ app/premium/page.tsx
@@ -35,13 +35,20 @@ const CSR = () => {
 
   return (
     <div className={"w-full h-full p-2 md:p-4 relative"}>
-      <div className={"p-2 md:p-8 md:border rounded-lg"}>
-        {/* @ts-ignore */}
-        <stripe-pricing-table
-          publishable-key="pk_live_51MagF9FPpv8QfieYNE5ZeIQQTIFXZxdRHKyHo8xmWXkYKnYyXKoaxh6BEs5JuNIpgAWU5FNt7d5gyhRrVsxFtqxL00vTDy0spV"
-          pricing-table-id="prctbl_1ON4GZFPpv8QfieYARNF5SMG"
-          customer-email={data.customer.email}
-        />
+      <div
+        className={"p-2 md:p-8 md:border rounded-lg space-y-4 xl:space-y-16"}
+      >
+        <div className={"text-2xl xl:text-3xl font-semibold text-center"}>
+          Affordable plans for any situation
+        </div>
+        <div>
+          {/* @ts-ignore */}
+          <stripe-pricing-table
+            publishable-key="pk_live_51MagF9FPpv8QfieYNE5ZeIQQTIFXZxdRHKyHo8xmWXkYKnYyXKoaxh6BEs5JuNIpgAWU5FNt7d5gyhRrVsxFtqxL00vTDy0spV"
+            pricing-table-id="prctbl_1ON4GZFPpv8QfieYARNF5SMG"
+            customer-email={data.customer.email}
+          />
+        </div>
       </div>
       <Link href={`/chat/${dysortid()}`}>
         <div className={"text-center absolute bottom-4 w-full underline"}>
