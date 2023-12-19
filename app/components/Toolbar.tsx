56c5eef8e95c3fcaac207e67d544229dced8735a
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-19 20:40:14 +0800

    disable Premium

--- app/components/Toolbar.tsx
+++ app/components/Toolbar.tsx
@@ -2,16 +2,11 @@
 import React, { FC } from "react";
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import useSWR from "swr";
 import SettingPopover from "@/app/components/SettingPopover";
 
 const Toolbar: FC<{ border?: boolean; children?: React.ReactNode }> = (
   props,
 ) => {
-  const { data, isLoading } = useSWR("/api/customer", (url) =>
-    fetch(url).then((res) => res.json()),
-  );
-
   return (
     <div
       className={`hidden h-[60px] w-full md:flex items-center justify-between px-6 xl:px-10 gap-8 ${
@@ -22,17 +17,6 @@ const Toolbar: FC<{ border?: boolean; children?: React.ReactNode }> = (
         className={"text-sm font-semibold flex items-center space-x-1 w-full"}
       >
         <div className={"w-full"}>{props?.children}</div>
-        {!isLoading && !data?.subscription?.isPremium && (
-          <Link
-            href={"/premium"}
-            prefetch
-            className={
-              "hover:shadow-lg px-4 py-2 text-sm rounded-full bg-gradient-to-tr from-[#AB68FF] to-[#0066FF] text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 whitespace-nowrap"
-            }
-          >
-            Explore Premium
-          </Link>
-        )}
         <Link
           href={"https://github.com/tunogya/abandonai-app/discussions"}
           target={"_blank"}
