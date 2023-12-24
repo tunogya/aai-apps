14bbfa9040c96242cb3ec05a99305ece609570d6
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-24 23:26:15 +0800

    update

--- app/api/bot/[id]/route.ts
+++ app/api/bot/[id]/route.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import redisClient from "@/app/utils/redisClient";
 import OpenAI from "openai";
+import sqsClient from "@/app/utils/sqsClient";
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
 
 const POST = async (req: NextRequest, { params }: any) => {
   const body = await req.json();
 
   const token = params.id;
+  // Check assistant_id
   const assistant_id = await redisClient.get(`${token}:assistant_id`);
   if (!assistant_id) {
     return NextResponse.json(
@@ -13,13 +16,16 @@ const POST = async (req: NextRequest, { params }: any) => {
       { status: 404 },
     );
   }
+
   const openai = new OpenAI();
   const from_id = body?.message?.from?.id;
 
+  // Check thread, if not exist, create
   let thread_id = await redisClient.get(
     `${assistant_id}:telegram:${from_id}:thread_id`,
   );
 
+  // Create new thread
   if (!thread_id) {
     const { id } = await openai.beta.threads.create();
     thread_id = id;
@@ -29,10 +35,27 @@ const POST = async (req: NextRequest, { params }: any) => {
     );
   }
 
+  // Add messages to thread
   await openai.beta.threads.messages.create(thread_id as string, {
     role: "user",
     content: JSON.stringify(body),
   });
+
+  await sqsClient.send(
+    new SendMessageCommand({
+      QueueUrl: process.env.AI_ASST_SQS_FIFO_URL,
+      MessageBody: JSON.stringify({
+        thread_id,
+        assistant_id,
+      }),
+      MessageAttributes: {
+        intent: {
+          StringValue: "run",
+          DataType: "String",
+        },
+      },
+    }),
+  );
 
   return NextResponse.json({});
 };
