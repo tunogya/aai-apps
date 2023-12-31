bb59858c9bb10d3581013d2a3aa82fca2c39aa2d
tunogya <20251942+tunogya@users.noreply.github.com>
2023-12-31 23:33:48 +0800

    update

--- app/billing/page.tsx
+++ app/billing/page.tsx
@@ -3,6 +3,7 @@
 import useSWR from "swr";
 import React from "react";
 import CheckoutButton from "@/app/components/CheckoutButton";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 
 const CSR = () => {
   const { data, isLoading } = useSWR("/api/billing", (url) =>
@@ -32,30 +33,54 @@ const CSR = () => {
           Pending invoice
         </div>
         <div className={"text-3xl text-gray-800"}>$0.00</div>
-        <div className={"text-gray-500"}>
+        <div className={"pb-4 text-gray-500"}>
           You&apos;ll be billed at the end of each calendar month for usage
           during that month.
         </div>
+        <div className={"flex w-full gap-2"}>
+          <CheckoutButton
+            price={process.env.NEXT_PUBLIC_AAI_CREDIT_PRICE!}
+            title={"Buy AAI credit"}
+            className={
+              "bg-gray-100 text-gray-800 rounded w-fit px-3 py-1.5 font-medium cursor-pointer text-sm"
+            }
+          />
+          {data?.session?.url && (
+            <a href={data.session.url} className={""}>
+              <div
+                className={
+                  "bg-gray-100 text-gray-800 rounded w-fit px-3 py-1.5 font-medium cursor-pointer text-sm"
+                }
+              >
+                Manage Billing
+              </div>
+            </a>
+          )}
+        </div>
       </div>
-      <div className={"flex w-full gap-2"}>
-        <CheckoutButton
-          price={process.env.NEXT_PUBLIC_AAI_CREDIT_PRICE!}
-          title={"Buy AAI"}
-          className={
-            "bg-gray-100 text-gray-800 rounded w-fit px-3 py-1.5 font-medium cursor-pointer text-sm"
-          }
-        />
-        {data?.session?.url && (
-          <a href={data.session.url} className={""}>
+      <div className={"pt-8"}>
+        <div className={"p-4 border w-fit min-w-[400px] rounded space-y-2"}>
+          <div className={"text-lg font-semibold text-gray-800"}>
+            My balance
+          </div>
+          <div className={"text-3xl text-gray-800"}>0.00 AAI</div>
+          <div className={"flex gap-2"}>
             <div
               className={
-                "bg-gray-100 text-gray-800 rounded w-fit px-3 py-1.5 font-medium cursor-pointer text-sm"
+                "px-3 py-1.5 bg-gray-100 text-gray-800 rounded text-sm font-medium flex items-center gap-1"
               }
             >
-              Manage Billing
+              <div>Deposit</div>
             </div>
-          </a>
-        )}
+            <div
+              className={
+                "px-3 py-1.5 bg-gray-100 text-gray-800 rounded text-sm font-medium flex items-center gap-1"
+              }
+            >
+              <div>Withdraw</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
