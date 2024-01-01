e47829b531256db06de0f65cf823e871d8a32d8d
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-01 18:17:01 +0800

    update

--- app/api/subscription/route.ts
+++ app/api/subscription/route.ts
@@ -94,4 +94,64 @@ const GET = async (req: NextRequest) => {
   return NextResponse.json(subscription);
 };
 
-export { GET };
+const POST = async (req: NextRequest) => {
+  // @ts-ignore
+  const { user } = await getSession();
+  // get data from cache, if exists, must be premium user
+  const customer = await redisClient.get(`customer:${user.email}`);
+  // @ts-ignore
+  if (!customer || !customer?.id) {
+    return NextResponse.json(
+      {
+        error: "customer not found",
+        message: "Please use email to login.",
+      },
+      {
+        status: 404,
+      },
+    );
+  }
+  if (!user.email) {
+    return NextResponse.json({
+      error: "email required",
+      message: "Please use email to login.",
+    });
+  }
+
+  const cache = await redisClient.get(`subscription:${user.email}`);
+
+  if (cache) {
+    return NextResponse.json(cache);
+  } else {
+    const subscription = await stripeClient.subscriptions.create({
+      // @ts-ignore
+      customer: customer.id,
+      description: "Pay as you go",
+      items: [
+        {
+          price: process.env.NEXT_PUBLIC_GPT_4_INPUT_PRICE,
+        },
+        {
+          price: process.env.NEXT_PUBLIC_GPT_4_OUTPUT_PRICE,
+        },
+        {
+          price: process.env.NEXT_PUBLIC_GPT_3_5_INPUT_PRICE,
+        },
+        {
+          price: process.env.NEXT_PUBLIC_GPT_3_5_OUTPUT_PRICE,
+        },
+        {
+          price: process.env.NEXT_PUBLIC_DALLE_3_PRICE,
+        },
+      ],
+      billing_thresholds: {
+        amount_gte: 2000,
+        reset_billing_cycle_anchor: true,
+      },
+    });
+    await redisClient.set(`subscription:${user.email}`, subscription);
+    return NextResponse.json(subscription);
+  }
+};
+
+export { GET, POST };
