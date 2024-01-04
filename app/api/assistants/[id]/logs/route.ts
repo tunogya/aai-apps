8adb764708c4ebd890d4ba9fd11a5af0801c414b
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-04 22:28:21 +0800

    update

--- app/api/assistants/[id]/logs/route.ts
+++ app/api/assistants/[id]/logs/route.ts
@@ -1,11 +1,8 @@
-import { getSession } from "@auth0/nextjs-auth0";
 import { NextRequest, NextResponse } from "next/server";
 import ddbDocClient from "@/app/utils/ddbDocClient";
 import { QueryCommand } from "@aws-sdk/lib-dynamodb";
 
 const GET = async (req: NextRequest, { params }: any) => {
-  const session = await getSession();
-  const sub = session?.user.sub;
   const limit = Number(req?.nextUrl?.searchParams?.get("limit") || 20);
   const cursor = req?.nextUrl?.searchParams?.get("cursor") || undefined;
   const { Items, Count, LastEvaluatedKey } = await ddbDocClient.send(
@@ -18,14 +15,14 @@ const GET = async (req: NextRequest, { params }: any) => {
       },
       ExpressionAttributeValues: {
         ":pk": `ASST#${params.id}`,
-        ":sk": "THREAD#",
+        ":sk": "RUN#",
       },
       Limit: limit,
       ScanIndexForward: false,
       ExclusiveStartKey: cursor
         ? {
             PK: `ASST#${params.id}`,
-            SK: `THREAD#${cursor}`,
+            SK: `RUN#${cursor}`,
           }
         : undefined,
     }),
@@ -33,7 +30,7 @@ const GET = async (req: NextRequest, { params }: any) => {
   return NextResponse.json({
     items: Items,
     count: Count,
-    nextCursor: LastEvaluatedKey?.SK.replace("THREAD#", "") || undefined,
+    nextCursor: LastEvaluatedKey?.SK.replace("RUN#", "") || undefined,
   });
 };
 
