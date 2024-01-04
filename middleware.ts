d6e131a12bfe9aa1caef6650294cd4328a89bc2d
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-05 01:18:14 +0800

    update

--- middleware.ts
+++ middleware.ts
@@ -2,18 +2,5 @@ import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: [
-    "/api/assistants/:path*",
-    "/api/audio/:path*",
-    "/api/chat/:path*",
-    "/api/checkout",
-    "/api/customer",
-    "/api/files",
-    "/api/images/:path*",
-    "/api/moderations",
-    "/api/serper/:path*",
-    "/billing/:path*",
-    "/chat/:path*",
-    "/assistants/:path*",
-  ],
+  matcher: ["/billing/:path*", "/chat/:path*", "/assistants/:path*"],
 };
