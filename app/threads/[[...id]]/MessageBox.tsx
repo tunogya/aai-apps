0f02a6cec21add1e1c4035209736f9d3d094d10b
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-04 23:40:58 +0800

    update

--- app/threads/[[...id]]/MessageBox.tsx
+++ app/threads/[[...id]]/MessageBox.tsx
@@ -25,46 +25,50 @@ const MessageBox: FC<{
 
   return (
     <div
-      className={`border-b p-5 flex flex-col ${
+      className={`border-b p-5 flex justify-center ${
         message?.role === "user" ? "bg-gray-50" : ""
       }`}
     >
-      <div className={"text-sm text-gray-500 flex justify-between mb-1"}>
-        {isTelegram ? (
-          <Link href={`https://t.me/${username}`} target={"_blank"}>
-            @{username}
-          </Link>
-        ) : (
-          <div>{message?.role}</div>
-        )}
-        <div className={"text-gray-400 text-[13px]"}>
-          {new Date(message?.created_at * 1000).toLocaleString()}
-        </div>
-      </div>
-      {message.content?.[0].type === "text" &&
-        (isTelegram ? (
-          <div className={"text-gray-600 break-words flex gap-1 items-center"}>
-            {JSON.parse(message.content[0].text.value).message.text}
+      <div className={"flex flex-col w-full max-w-3xl"}>
+        <div className={"text-sm text-gray-500 flex justify-between mb-1"}>
+          {isTelegram ? (
+            <Link href={`https://t.me/${username}`} target={"_blank"}>
+              @{username}
+            </Link>
+          ) : (
+            <div>{message?.role}</div>
+          )}
+          <div className={"text-gray-400 text-[13px]"}>
+            {new Date(message?.created_at * 1000).toLocaleString()}
           </div>
-        ) : (
+        </div>
+        {message.content?.[0].type === "text" &&
+          (isTelegram ? (
+            <div
+              className={"text-gray-600 break-words flex gap-1 items-center"}
+            >
+              {JSON.parse(message.content[0].text.value).message.text}
+            </div>
+          ) : (
+            <div className={"text-gray-600 break-words"}>
+              {message.content[0].text.value}
+            </div>
+          ))}
+        {message.content?.[0].type === "image_file" && (
           <div className={"text-gray-600 break-words"}>
-            {message.content[0].text.value}
+            file_id: {message.content[0].image_file.file_id}
           </div>
-        ))}
-      {message.content?.[0].type === "image_file" && (
-        <div className={"text-gray-600 break-words"}>
-          file_id: {message.content[0].image_file.file_id}
-        </div>
-      )}
-      {message?.run_id && (
-        <div
-          className={
-            "text-xs border px-2 py-1 text-gray-800 w-fit rounded mt-2 bg-gray-100"
-          }
-        >
-          run_id: {message.run_id}
-        </div>
-      )}
+        )}
+        {message?.run_id && (
+          <div
+            className={
+              "text-xs border px-2 py-1 text-gray-800 w-fit rounded mt-2 bg-gray-100"
+            }
+          >
+            run_id: {message.run_id}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
