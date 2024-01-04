899b74463a42ac19d4c882b506c36230d6556b5c
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-05 01:05:57 +0800

    update

--- app/components/Toolbar.tsx
+++ app/components/Toolbar.tsx
@@ -3,10 +3,16 @@ import React, { FC } from "react";
 import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import SettingPopover from "@/app/components/SettingPopover";
+import useSWR from "swr";
+import Skeleton from "react-loading-skeleton";
 
 const Toolbar: FC<{ border?: boolean; children?: React.ReactNode }> = (
   props,
 ) => {
+  const { data } = useSWR("/api/customer", (url) =>
+    fetch(url).then((res) => res.json()),
+  );
+
   return (
     <div
       className={`hidden h-[60px] w-full md:flex items-center justify-between px-6 xl:px-10 gap-8 ${
@@ -29,6 +35,20 @@ const Toolbar: FC<{ border?: boolean; children?: React.ReactNode }> = (
           />
         </Link>
         <SettingPopover />
+        {data && data?.currency === "usd" ? (
+          <Link
+            href={"/billing"}
+            className={`flex items-center w-fit min-w-24 border whitespace-nowrap px-2 h-8 hover:bg-gray-100 rounded-lg ${
+              data.balance >= 0 ? "text-red-500" : "text-gray-600"
+            }`}
+          >
+            {(data?.balance * -1) / 100} AAI
+          </Link>
+        ) : (
+          <div className={"w-24 px-2 h-8"}>
+            <Skeleton className={"h-6"} />
+          </div>
+        )}
       </div>
     </div>
   );
