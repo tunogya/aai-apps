84a1cd93b644294b5e406c74b914983d1f8ebd34
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-29 13:49:59 +0800

    add audience

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,4 +1,11 @@
-import { handleAuth } from "@auth0/nextjs-auth0/edge";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0/edge";
 export const runtime = "edge";
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  login: handleLogin({
+    authorizationParams: {
+      audience: process.env.AUTH0_AUDIENCE,
+      scope: process.env.AUTH0_SCOPE,
+    },
+  }),
+});
