e3b83acc79b7e18ccda8e4c3dcef3640912714dd
tunogya <20251942+tunogya@users.noreply.github.com>
2024-02-13 16:46:26 +0800

    add threads

---
+++ app/api/threads/route.ts
@@ -0,0 +1,84 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "@auth0/nextjs-auth0";
+import ddbDocClient from "@/app/utils/ddbDocClient";
+import { PutCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+import openai from "@/app/utils/openai";
+import redisClient from "@/app/utils/redisClient";
+
+const GET = async (req: NextRequest) => {
+  const session = await getSession();
+  const sub = session?.user.sub;
+  const limit = Number(req?.nextUrl?.searchParams?.get("limit") || 20);
+  const cursor = req?.nextUrl?.searchParams?.get("cursor") || undefined;
+  try {
+    const { Items, Count, LastEvaluatedKey } = await ddbDocClient.send(
+      new QueryCommand({
+        TableName: "abandonai-prod",
+        KeyConditionExpression: "#pk = :pk AND begins_with(#sk, :sk)",
+        ExpressionAttributeValues: {
+          ":pk": `USER#${sub}`,
+          ":sk": "THREAD#",
+        },
+        ExpressionAttributeNames: {
+          "#pk": "PK",
+          "#sk": "SK",
+        },
+        Limit: limit,
+        ScanIndexForward: false,
+        ExclusiveStartKey: cursor
+          ? {
+              PK: `USER#${sub}`,
+              SK: `THREAD#${cursor}`,
+            }
+          : undefined,
+      }),
+    );
+    return NextResponse.json({
+      items: Items,
+      count: Count,
+      nextCursor: LastEvaluatedKey?.SK.replace("THREAD#", "") || undefined,
+    });
+  } catch (e) {
+    return NextResponse.json(
+      {
+        error: "ddbDocClient error",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+};
+
+const POST = async (req: NextRequest) => {
+  const session = await getSession();
+  const sub = session?.user.sub;
+  try {
+    const newThread = await openai.beta.threads.create();
+    const item = {
+      ...newThread,
+      PK: `USER#${sub}`,
+      SK: `THREAD#${newThread.id}`,
+    };
+
+    await redisClient.set(`THREAD#${newThread.id}`, JSON.stringify(item));
+    await ddbDocClient.send(
+      new PutCommand({
+        TableName: "abandonai-prod",
+        Item: item,
+        ConditionExpression:
+          "attribute_not_exists(PK) AND attribute_not_exists(SK)",
+      }),
+    );
+    return NextResponse.json({
+      success: true,
+      item,
+    });
+  } catch (e) {
+    return NextResponse.json({
+      error: "ddbDocClient error",
+    });
+  }
+};
+
+export { GET, POST };
