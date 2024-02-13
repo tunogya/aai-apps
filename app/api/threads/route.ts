19c499cb4c7745b1b55b438f43096a41a4dc4ea6
tunogya <20251942+tunogya@users.noreply.github.com>
2024-02-13 22:33:16 +0800

    update

--- app/api/threads/route.ts
+++ app/api/threads/route.ts
@@ -4,10 +4,32 @@ import ddbDocClient from "@/app/utils/ddbDocClient";
 import { PutCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import openai from "@/app/utils/openai";
 import redisClient from "@/app/utils/redisClient";
+import jwt from "jsonwebtoken";
+import AUTH0_CERT from "@/app/utils/cert";
 
 const GET = async (req: NextRequest) => {
   const session = await getSession();
-  const sub = session?.user.sub;
+  let sub = session?.user.sub;
+  if (!sub) {
+    // get from header Authorization
+    let token = req.headers.get("Authorization");
+    if (!token) {
+      return NextResponse.json(
+        {
+          error: "auth error",
+        },
+        {
+          status: 500,
+        },
+      );
+    }
+    token = token.split(" ")[1];
+    const decodedToken = jwt.verify(token, AUTH0_CERT, {
+      algorithms: ["RS256"],
+    });
+    sub = decodedToken.sub;
+  }
+
   const limit = Number(req?.nextUrl?.searchParams?.get("limit") || 20);
   const cursor = req?.nextUrl?.searchParams?.get("cursor") || undefined;
   try {
