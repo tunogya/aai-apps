a079b81cc4385aa97582550a6a3dcf4ed47aab7a
tunogya <20251942+tunogya@users.noreply.github.com>
2024-02-25 18:18:55 +0800

    update

--- app/components/AAI.tsx
+++ app/components/AAI.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { OpenAI } from "@lobehub/icons";
 import React from "react";
 
 const AAI = () => {
@@ -12,27 +11,34 @@ const AAI = () => {
       >
         <div
           className={
-            "w-28 h-28 md:w-52 md:h-52 bg-pink-600 p-1 md:p-3 cursor-pointer rounded-lg relative overflow-hidden"
+            "w-28 h-28 md:w-52 md:h-52 bg-[#d3373c] p-1 md:p-3 cursor-pointer rounded-lg relative overflow-hidden"
           }
         >
           <div className={"text-md md:text-2xl font-semibold text-white"}>
             AAI Coin
           </div>
           <div
-            className={"absolute right-0 bottom-0 group-hover:animate-pulse"}
+            className={
+              "absolute right-[-20px] bottom-[-20px] md:right-[-24px] md:bottom-[-24px] p-1 md:p-2 rotate-45 shadow-xl shadow-black/30 w-20 h-20 md:w-28 md:h-28"
+            }
+            style={{
+              backgroundImage:
+                "url(https://nftstorage.link/ipfs/bafkreie2yh7h2k2lvkqjepk2cck5gszwwwopb4pvhbsmalyhbkmgnm7kye)",
+              backgroundSize: "cover",
+            }}
           >
-            <svg
-              width="120"
-              height="120"
-              viewBox="0 0 1024 1024"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M68 68V956H956V68H68ZM142 882V142H586V413.333L512 216H438L216 808H290L345.5 660H586V882H142ZM576.791 586H373.209L475 314.667L576.791 586Z"
-                fill="white"
-              />
-            </svg>
+            <div className={"md:w-10 md:h-10 h-8 w-8"}>
+              <svg
+                viewBox="0 0 1024 1024"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M68 68V956H956V68H68ZM142 882V142H586V413.333L512 216H438L216 808H290L345.5 660H586V882H142ZM576.791 586H373.209L475 314.667L576.791 586Z"
+                  fill="#121212"
+                />
+              </svg>
+            </div>
           </div>
         </div>
         <div className={"flex flex-col flex-1 md:w-52 gap-2"}>
--- app/components/AbandonAI.tsx
+++ app/components/AbandonAI.tsx
@@ -12,7 +12,7 @@ const AbandonAI = () => {
       >
         <div
           className={
-            "w-28 h-28 md:w-52 md:h-52 bg-green-600 p-1 md:p-3 cursor-pointer rounded-lg relative overflow-hidden"
+            "w-28 h-28 md:w-52 md:h-52 bg-[#40b840] p-1 md:p-3 cursor-pointer rounded-lg relative overflow-hidden"
           }
         >
           <div className={"text-md md:text-2xl font-semibold text-white"}>
@@ -20,10 +20,15 @@ const AbandonAI = () => {
           </div>
           <div
             className={
-              "absolute right-[-40px] bottom-[-40px] group-hover:animate-pulse"
+              "absolute right-[-20px] bottom-[-20px] md:right-[-24px] md:bottom-[-24px] p-1 md:p-2 rotate-45 shadow-xl shadow-black/30 w-20 h-20 md:w-28 md:h-28"
             }
+            style={{
+              backgroundImage:
+                "url(https://nftstorage.link/ipfs/bafkreialozci4nzjme7a4jjjfwelogn42klvuwiy7sdjeyyivsejuuu3fm)",
+              backgroundSize: "cover",
+            }}
           >
-            <OpenAI className={"w-28 h-28 md:w-40 md:h-40"} color={"white"} />
+            <OpenAI className={"md:w-10 md:h-10 w-8 h-8"} color={"#121212"} />
           </div>
         </div>
         <div className={"flex flex-col flex-1 md:w-52 gap-2"}>
