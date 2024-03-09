61f8a4ef0d56294358f3867cf4b857dc5e9b7e96
tunogya <20251942+tunogya@users.noreply.github.com>
2024-03-10 02:11:50 +0800

    update

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -2,10 +2,5 @@ import { handleAuth, handleLogin } from "@auth0/nextjs-auth0/edge";
 export const runtime = "edge";
 
 export const GET = handleAuth({
-  login: handleLogin({
-    authorizationParams: {
-      audience: process.env.AUTH0_AUDIENCE,
-      scope: process.env.AUTH0_SCOPE,
-    },
-  }),
+  login: handleLogin({}),
 });
--- app/wallet/LoginView.tsx
+++ app/wallet/LoginView.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import TheFooter from "@/app/components/TheFooter";
 
 const LoginView = () => {
   return (
